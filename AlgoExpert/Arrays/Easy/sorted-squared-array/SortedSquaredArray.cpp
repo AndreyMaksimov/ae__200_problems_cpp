ddab659458d5830374036493a2097770bba8f182
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-06-23 22:27:28 +0300

    My 1-st sortedSquaredArray implementation

--- AlgoExpert/Arrays/Easy/sorted-squared-array/SortedSquaredArray.cpp
+++ AlgoExpert/Arrays/Easy/sorted-squared-array/SortedSquaredArray.cpp
@@ -5,8 +5,40 @@
 
 namespace algoExpert::arrays {
     std::vector<int> sortedSquaredArray(std::vector<int> array) {
-        // Write your code here.
-        return {};
-    }
+        const auto size = array.size();
+        std::vector<int> array_result(size);
+        auto pos_result = array_result.begin();
 
+        auto i0 = 0;
+        while (array[i0] < 0) {
+            i0++;
+        }
+
+        auto iter_r = array.rbegin() + size - i0;
+        auto iter_f = array.begin() + i0;
+
+        while ( iter_r != array.rend() || iter_f != array.end()) {
+            int r;
+            if (iter_r != array.rend() && iter_f != array.end()) {
+                const auto a_neg = -(*iter_r);
+                const auto a_pos = *iter_f;
+                if (a_pos > a_neg) {
+                    r = a_neg;
+                    ++iter_r;
+                }
+                else {
+                    r = a_pos;
+                    ++iter_f;
+                }
+            }
+            else if (iter_r != array.rend()) {
+                r = -(*iter_r++);
+            }
+            else {
+                r = *iter_f++;
+            }
+            *pos_result++ = r*r;
+        }
+        return array_result;
+    }
 };
--- AlgoExpert/Arrays/Easy/sorted-squared-array/SortedSquaredArray_test.cpp
+++ AlgoExpert/Arrays/Easy/sorted-squared-array/SortedSquaredArray_test.cpp
@@ -1,5 +1,19 @@
 #include "SortedSquaredArray.h"
+#include "gtest/gtest.h"
 
 namespace {
-
+    TEST(SortedSquaredArray, Case01)
+    {
+        const std::vector<int> array = {1, 2, 3, 5, 6, 8, 9};
+        const std::vector<int> expected = {1, 4, 9, 25, 36, 64, 81};
+        const std::vector<int> output = algoExpert::arrays::sortedSquaredArray(array);
+        EXPECT_EQ(expected, output);
+    }
+    TEST(SortedSquaredArray, Case12)
+    {
+        const std::vector<int> array = {-7, -3, 1, 9, 22, 30};
+        const std::vector<int> expected = {1, 9, 49, 81, 484, 900};
+        const std::vector<int> output = algoExpert::arrays::sortedSquaredArray(array);
+        EXPECT_EQ(expected, output);
+    }
 }
