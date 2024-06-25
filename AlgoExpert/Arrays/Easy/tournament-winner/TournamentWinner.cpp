bd6fe73ffd29bfec4417779829ba55bce8546045
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-06-25 18:10:59 +0300

    tournament-winner Added my solution

--- AlgoExpert/Arrays/Easy/tournament-winner/TournamentWinner.cpp
+++ AlgoExpert/Arrays/Easy/tournament-winner/TournamentWinner.cpp
@@ -1,12 +1,41 @@
+#include <map>
+#include <utility>
+#include <algorithm>
 #include "TournamentWinner.h"
 
 namespace algoExpert::arrays {
-    using std::string, std::vector;
+    using std::string, std::vector, std::map, std::make_pair, std::max;
 
     string tournamentWinner(
             vector<vector<string>> competitions,
             vector<int> results)
     {
-        return "";
+        if (competitions.empty() || results.empty()) return "";
+        if (competitions.size() != results.size()) return "";
+
+        map<string, int> team2wins;
+        int max_wins = 0;
+        string team_first_place;
+        auto pos_r = results.begin();
+        for (const auto & game : competitions) {
+            const auto team_win_id = 1 - *pos_r++;
+            const auto& team_win = game[team_win_id];
+            auto tw_pair_pos = team2wins.find(team_win);
+            if (tw_pair_pos == team2wins.end()) {
+                team2wins.insert(make_pair(team_win, 1));
+                if (max_wins < 1) {
+                    max_wins = 1;
+                    team_first_place = team_win;
+                }
+            }
+            else {
+                tw_pair_pos->second++;
+                if (max_wins < tw_pair_pos->second) {
+                    max_wins = tw_pair_pos->second;
+                    team_first_place = team_win;
+                }
+            }
+        }
+        return team_first_place;
     }
 }
