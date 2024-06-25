ad7f16a92d5f4a8ca9319a0f1a3f705fd8d6d9ab
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-06-25 21:29:53 +0300

    non-constructible-change Added my solution

--- AlgoExpert/Arrays/Easy/non-constructible-change/NonConstructibleChange.cpp
+++ AlgoExpert/Arrays/Easy/non-constructible-change/NonConstructibleChange.cpp
@@ -1,10 +1,27 @@
+// https://www.algoexpert.io/questions/non-constructible-change
+//
+// #std::sort
+
+#include <algorithm>
 #include "NonConstructibleChange.h"
 
 namespace algoExpert::arrays {
-    using std::vector;
+    using std::vector, std::sort;
 
     int nonConstructibleChange(vector<int> coins) {
-        // Write your code here.
-        return -1;
+        if(coins.empty()) return 1;
+        const auto size = coins.size();
+        if (size == 1) return coins[0] > 1 ? 1 : 2;
+
+        sort(coins.begin(), coins.end());
+        if (coins[0] > 1) return 1;
+
+        int sum = 0;
+        for(int i = 0; i < (size-1); ++i) {
+            sum += coins[i];
+            if ((sum+1) < coins[i+1]) return (sum+1);
+        }
+
+        return sum + coins[size-1] + 1;
     }
 };
