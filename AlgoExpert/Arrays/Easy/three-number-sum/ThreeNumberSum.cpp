d6d3bde60d9ccebd0a0eefc3d96bc85c8ba00b60
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-06-26 20:05:50 +0300

    three-number-sum Added my solutions

--- AlgoExpert/Arrays/Easy/three-number-sum/ThreeNumberSum.cpp
+++ AlgoExpert/Arrays/Easy/three-number-sum/ThreeNumberSum.cpp
@@ -3,11 +3,35 @@
 // #Arrays
 // #Easy
 
+#include <algorithm>
+#include <set>
 #include "ThreeNumberSum.h"
 
 namespace algoExpert::arrays {
+    using std::sort, std::set;
     vector<vector<int>> threeNumberSum(vector<int> array, int targetSum) {
-        // Write your code here.
-        return {};
+        const auto size = array.size();
+        if (size < 3) return {};
+
+        sort(array.begin(), array.end());
+        vector<vector<int>> result;
+
+        for (int i=0; i<(size-2); ++i) {
+            const int n1 = array[i];
+            if ((n1 + n1 + n1 + 2) > targetSum) break;
+            for(int j=(i+1); j<(size-1); ++j) {
+                const int n2 = array[j];
+                if ((n1 + n2 + 1) > targetSum) break;
+                for (int k=(j+1); k<size; ++k) {
+                    const int n3 = array[k];
+                    if ( (n1+n2+n3) == targetSum ) {
+                        result.push_back({n1, n2, n3});
+                    }
+                    if ((n1+n2+n3) >= targetSum) break;
+                }
+            }
+        }
+
+        return result;
     }
 }
