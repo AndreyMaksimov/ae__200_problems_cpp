010baf4caf71e6836539794fad0a0b4b340a9391
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-06-26 20:45:02 +0300

    three-number-sum Added optimal O(n^2) solution

--- AlgoExpert/Arrays/Easy/three-number-sum/ThreeNumberSum.cpp
+++ AlgoExpert/Arrays/Easy/three-number-sum/ThreeNumberSum.cpp
@@ -2,6 +2,7 @@
 //
 // #Arrays
 // #Easy
+// #std::sort
 
 #include <algorithm>
 #include <set>
@@ -9,7 +10,7 @@
 
 namespace algoExpert::arrays {
     using std::sort, std::set;
-    vector<vector<int>> threeNumberSum(vector<int> array, int targetSum) {
+    vector<vector<int>> threeNumberSum_n3(vector<int>& array, int targetSum) {
         const auto size = array.size();
         if (size < 3) return {};
 
@@ -31,7 +32,36 @@ namespace algoExpert::arrays {
                 }
             }
         }
+        return result;
+    }
+    vector<vector<int>> threeNumberSum_n2(vector<int>& array, int targetSum) {
+        const auto size = array.size();
+        if (size < 3) return {};
+
+        sort(array.begin(), array.end());
+        vector<vector<int>> result;
+
+        for (int i=0; i<(size-2); ++i) {
+            int j = i + 1;
+            int k = static_cast<int>(size) - 1;
+            while (j != k) {
+                const int n1 = array[i];
+                const int n2 = array[j];
+                const int n3 = array[k];
+                const int sum = n1 + n2 + n3;
+                if (sum == targetSum) {
+                    result.push_back({n1, n2, n3});
+                    ++j; // or --k; // doesn't matter
+                }
+                else if (sum > targetSum) --k;
+                else ++j;
+            }
+        }
 
         return result;
+    }
+    vector<vector<int>> threeNumberSum(vector<int> array, int targetSum) {
+//        return threeNumberSum_n3(array, targetSum);
+        return threeNumberSum_n2(array, targetSum);
     }
 }
