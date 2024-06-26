76dd4470dacf05153f996e46439f58007a9a2699
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-06-26 13:37:10 +0300

    transpose-matrix Added function sketch

--- AlgoExpert/Arrays/Easy/transpose-matrix/TransposeMatrix.cpp
+++ AlgoExpert/Arrays/Easy/transpose-matrix/TransposeMatrix.cpp
@@ -1,4 +1,9 @@
 #include "TransposeMatrix.h"
 
 namespace algoExpert::arrays {
+    using std::vector;
+    vector<vector<int>> transposeMatrix(vector<vector<int>> matrix) {
+        // Write your code here.
+        return {};
+    }
 }
--- AlgoExpert/Arrays/Easy/transpose-matrix/TransposeMatrix.h
+++ AlgoExpert/Arrays/Easy/transpose-matrix/TransposeMatrix.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <vector>
 
 namespace algoExpert::arrays {
+    std::vector<std::vector<int>> transposeMatrix(std::vector<std::vector<int>> matrix);
 }
