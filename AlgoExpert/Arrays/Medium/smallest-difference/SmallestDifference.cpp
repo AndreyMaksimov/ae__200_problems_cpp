837ada3ba8e5640a46a062cd22ffd5b886cd8c41
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-06-27 14:44:50 +0300

    smallest-difference Added Hint3 solution

--- AlgoExpert/Arrays/Medium/smallest-difference/SmallestDifference.cpp
+++ AlgoExpert/Arrays/Medium/smallest-difference/SmallestDifference.cpp
@@ -6,11 +6,13 @@
 
 #include <algorithm>
 #include <limits>
+#include <iostream>
 #include "SmallestDifference.h"
 
 namespace algoExpert::arrays {
     using std::sort, std::abs, std::max;
-    vector<int> smallestDifference(vector<int> arrayOne, vector<int> arrayTwo) {
+    using std::cout, std::endl;
+    vector<int> smallestDifference_my(vector<int> arrayOne, vector<int> arrayTwo) {
         sort(arrayOne.begin(), arrayOne.end());
         sort(arrayTwo.begin(), arrayTwo.end());
         const auto sizeOne= arrayOne.size();
@@ -40,5 +42,49 @@ namespace algoExpert::arrays {
             }
         }
         return result;
+    }
+
+    vector<int> smallestDifferenceHint3(vector<int> arrayOne, vector<int> arrayTwo) {
+        sort(arrayOne.begin(), arrayOne.end());
+        sort(arrayTwo.begin(), arrayTwo.end());
+        const auto sizeOne= arrayOne.size();
+        const auto sizeTwo= arrayTwo.size();
+        vector<int> result;
+        auto posOne = arrayOne.begin();
+        auto posTwo = arrayTwo.begin();
+
+        auto distance = std::numeric_limits<int>::max();
+        int a1, a2;
+        while (posOne != arrayOne.end() || posTwo != arrayTwo.end()) {
+            if (posOne != arrayOne.end()) a1 = *posOne;
+            if (posTwo != arrayTwo.end()) a2 = *posTwo;
+            if (a1 == a2) return {a1, a2};
+            const auto d = abs(a1 - a2);
+            if (d < distance) {
+                distance = d;
+                result = {a1, a2};
+            }
+            if (posOne == arrayOne.end()) {
+                ++posTwo;
+                continue;
+            }
+            else if (posTwo == arrayTwo.end()) {
+                ++posOne;
+                continue;
+            }
+            if (a1 < a2) {
+                ++posOne;
+            }
+            else {
+                ++posTwo;
+            }
+         }
+
+        return result;
+    }
+
+    vector<int> smallestDifference(vector<int> arrayOne, vector<int> arrayTwo) {
+//        return smallestDifference_my(arrayOne, arrayTwo);
+        return smallestDifferenceHint3(arrayOne, arrayTwo);
     }
 }
