4b165271d9b03acc194e32e47c4d03596426fc02
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-06-27 17:26:42 +0300

    move-element-to-end Added solution

--- AlgoExpert/Arrays/Medium/move-element-to-end/MoveElementToEnd.cpp
+++ AlgoExpert/Arrays/Medium/move-element-to-end/MoveElementToEnd.cpp
@@ -2,12 +2,23 @@
 //
 // #Arrays
 // #Medium
+// #std::swap
 
+#include <algorithm>
 #include "MoveElementToEnd.h"
 
 namespace algoExpert::arrays {
+    using std::swap;
     vector<int> moveElementToEnd(vector<int> array, int toMove) {
-        // Write your code here.
-        return {};
+        if (array.empty()) return {};
+        auto j = array.size() - 1;
+        decltype(j) i = 0;
+        while (true) {
+            while (array[i] != toMove && (i != j)) ++i;
+            while (array[j] == toMove && (i != j)) --j;
+            if (i == j) break;
+            else swap(array[i], array[j]);
+        }
+        return array;
     }
 }
