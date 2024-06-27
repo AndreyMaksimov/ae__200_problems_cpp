1e3f256425e5901b9c3d40f06174d03aa5ef6483
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-06-27 19:21:16 +0300

    monotonic-array Added solution

--- AlgoExpert/Arrays/Medium/monotonic-array/MonotonicArray.cpp
+++ AlgoExpert/Arrays/Medium/monotonic-array/MonotonicArray.cpp
@@ -7,7 +7,21 @@
 
 namespace algoExpert::arrays {
     bool isMonotonic(vector<int> array) {
-        // Write your code here.
-        return false;
+        if (array.empty() || array.size() == 1) return true;
+
+        auto pos = array.begin();
+        auto a_prev = *pos++;
+        int d_monotonic = 0;
+        while (pos != array.end()) {
+            auto a = *pos;
+            const auto d = a - a_prev;
+            if (d_monotonic == 0 && d !=0) {
+                d_monotonic = d;
+            }
+            else if (d_monotonic*d < 0) return false;
+            a_prev = a;
+            ++pos;
+        }
+        return true;
     }
 }
