cae7396bdf11a2f7b7188ee708770bd01b10d37a
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-03 16:56:41 +0300

    first-duplicate-value added solution

--- AlgoExpert/Arrays/Medium/first-duplicate-value/FirstDuplicateValue.cpp
+++ AlgoExpert/Arrays/Medium/first-duplicate-value/FirstDuplicateValue.cpp
@@ -3,10 +3,18 @@
 // #Arrays
 // #Medium
 
+#include <algorithm>
 #include "FirstDuplicateValue.h"
 
 namespace algoExpert::arrays {
     int firstDuplicateValue(vector<int> array) {
+        auto n = array.size();
+        for(auto i=0; i<array.size(); ++i) {
+            auto j =  std::abs(array[i]);
+            --j;
+            if (array[j] < 0) return j+1;
+            array[j] *= -1;
+        }
         return -1;
     }
 }
