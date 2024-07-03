1a2d38ac6fccfa86acaa2815a2d4f4d711347e4a
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-03 20:54:15 +0300

    merge-overlapping-intervals added solution

--- AlgoExpert/Arrays/Medium/merge-overlapping-intervals/MergeOverlappingIntervals.cpp
+++ AlgoExpert/Arrays/Medium/merge-overlapping-intervals/MergeOverlappingIntervals.cpp
@@ -2,12 +2,39 @@
 //
 // #Arrays
 // #Medium
+// #std::sort with custom comparator
 
+#include <algorithm>
 #include "MergeOverlappingIntervals.h"
 
 namespace algoExpert::arrays {
+    using std::sort, std::max;
     vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> intervals) {
-        // Write your code here.
-        return {};
+        if (intervals.size() < 2) return intervals;
+
+        struct
+        {
+            bool operator()(vector<int>& a, vector<int>& b) const {
+                return (a[0] < b[0]) ||
+                ((a[0] == b[0]) && (a[1] < b[1])); }
+        } customLess;
+        sort(intervals.begin(), intervals.end(), customLess);
+
+        vector<vector<int>> result;
+
+        auto pos = intervals.begin();
+        auto i0 = *pos++;
+        while (pos != intervals.end()) {
+            auto i1 = *pos++;
+            if (i1[0] <= i0[1]) { // extend current interval
+                i0[1] = max(i0[1],i1[1]);
+            }
+            else {
+                result.push_back(i0);
+                i0 = i1;
+            }
+        }
+        result.push_back(i0);
+        return result;
     }
 }
