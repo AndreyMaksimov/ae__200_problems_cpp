6f7abe1e82be9be824c4ee09510d39ec585f21ad
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-04 21:31:47 +0300

    zero-sum-subarray add solutions

--- AlgoExpert/Arrays/Medium/zero-sum-subarray/ZeroSumSubarray.cpp
+++ AlgoExpert/Arrays/Medium/zero-sum-subarray/ZeroSumSubarray.cpp
@@ -2,12 +2,22 @@
 //
 // #Arrays
 // #Medium
-
+#include <unordered_set>
 #include "ZeroSumSubarray.h"
 
 namespace algoExpert::arrays {
+    using std::unordered_set;
     bool zeroSumSubarray(vector<int> nums) {
-        // Write your code here.
+        if (nums.empty()) return false;
+
+        unordered_set<int> set_a;
+        int sum = 0;
+        for (auto n: nums) {
+            sum += n;
+            if (sum == 0) return true;
+            auto r = set_a.insert(sum);
+            if (!r.second) return true;
+        }
         return false;
     }
 }
