38e9ba3b85343e7e66ac3933467db9d79b37bcee
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-07 12:27:40 +0300

    Keep point

--- AlgoExpert/Arrays/Medium/sweet-and-savory/SweetAndSavory.cpp
+++ AlgoExpert/Arrays/Medium/sweet-and-savory/SweetAndSavory.cpp
@@ -3,11 +3,41 @@
 // #Arrays
 // #Medium
 
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <functional>
 #include "SweetAndSavory.h"
 
 namespace algoExpert::arrays {
+    using std::sort, std::abs, std::copy_if, std::back_inserter;
+    using std::cout, std::endl;
     vector<int> sweetAndSavory(vector<int> dishes, int target) {
-        // Write your code here.
-        return {};
+        const auto size = dishes.size();
+        vector<int> empty_result = {0, 0};
+        if (size < 2) return empty_result;
+
+        vector<int> sweets;
+        vector<int> savories;
+
+        // Create separate containers for sweets & savories
+        copy_if(dishes.begin(), dishes.end(),
+                back_inserter(sweets), [](int x){return (x < 0);});
+        if (sweets.empty()) return empty_result;
+        copy_if(dishes.begin(), dishes.end(),
+                back_inserter(savories), [](int x){return (x > 0);});
+        if (savories.empty()) return empty_result;
+
+        // Sort sweets & savories
+        sort(sweets.begin(), sweets.end(), std::greater<>());
+        sort(savories.begin(), savories.end());
+
+        vector<int> result = empty_result;
+
+        auto pos_sweet = sweets.begin();
+        auto pos_savory = sweets.begin();
+
+
+        return result;
     }
 }
