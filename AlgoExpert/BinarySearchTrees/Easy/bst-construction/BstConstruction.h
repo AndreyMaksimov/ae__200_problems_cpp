a31593e63f0b4f3c06bdf2cd71c76e4c2fce46c9
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-16 21:54:25 +0300

    bst-construction added "NotImplemented" note

--- AlgoExpert/BinarySearchTrees/Easy/bst-construction/BstConstruction.h
+++ AlgoExpert/BinarySearchTrees/Easy/bst-construction/BstConstruction.h
@@ -1,7 +1,39 @@
 #pragma once
 #include <vector>
-#include "BST.h"
+#include <stdexcept>
 
 namespace algoExpert::binarySearchTrees {
 	using std::vector;
+	class BST {
+	public:
+		int value;
+		BST* left;
+		BST* right;
+
+		BST(int val) {
+			value = val;
+			left = nullptr;
+			right = nullptr;
+		}
+
+		BST& insert(int val) {
+			// Write your code here.
+			throw std::logic_error("Not Implemented");
+			return *this;
+		}
+
+		bool contains(int val) {
+			// Write your code here.
+			throw std::logic_error("Not Implemented");
+			return false;
+		}
+
+		BST& remove(int val) {
+			// Write your code here.
+			// Do not edit the return statement of this method.
+			throw std::logic_error("Not Implemented");
+			return *this;
+		}
+	};
+
 }
