b1b2f775ce94620e2837f316adc529bef035b285
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-16 20:00:09 +0300

    find-closest-value-in-bst added solution

--- AlgoExpert/BinarySearchTrees/Easy/find-closest-value-in-bst/FindClosestValueInBst.cpp
+++ AlgoExpert/BinarySearchTrees/Easy/find-closest-value-in-bst/FindClosestValueInBst.cpp
@@ -3,10 +3,23 @@
 // #BinarySearchTrees
 // #Easy
 
+#include <cmath>
+#include <limits>
 #include "FindClosestValueInBst.h"
 
 namespace algoExpert::binarySearchTrees {
+    using std::abs;
     int findClosestValueInBst(int target, BST* tree) {
-        return -1;
+        int result = tree->value;
+        int min_diff = abs(result - target);
+        while (tree != nullptr) {
+            if (abs(tree->value - target) < min_diff) {
+                min_diff = abs(tree->value - target);
+                result = tree->value;
+            }
+            if (target >= tree->value) tree = tree->right;
+            else tree = tree->left;
+        }
+        return result;
     }
 }
