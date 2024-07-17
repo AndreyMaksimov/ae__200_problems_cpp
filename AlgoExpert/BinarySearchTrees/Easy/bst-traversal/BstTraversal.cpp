d380a0867bbc324c36abed5b52a5a3cdfe27f1e0
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-17 18:08:46 +0300

    bst-traversal added sloution

--- AlgoExpert/BinarySearchTrees/Easy/bst-traversal/BstTraversal.cpp
+++ AlgoExpert/BinarySearchTrees/Easy/bst-traversal/BstTraversal.cpp
@@ -6,4 +6,24 @@
 #include "BstTraversal.h"
 
 namespace algoExpert::binarySearchTrees {
+    void inOrderTraverse(BST* tree, vector<int>& array) {
+        if (!tree) return;
+        inOrderTraverse(tree->left, array);
+        array.push_back(tree->value);
+        inOrderTraverse(tree->right, array);
+    }
+
+    void preOrderTraverse(BST* tree, vector<int>& array) {
+        if (!tree) return;
+        array.push_back(tree->value);
+        preOrderTraverse(tree->left, array);
+        preOrderTraverse(tree->right, array);
+    }
+
+    void postOrderTraverse(BST* tree, vector<int>& array) {
+        if (!tree) return;
+        postOrderTraverse(tree->left, array);
+        postOrderTraverse(tree->right, array);
+        array.push_back(tree->value);
+    }
 }
--- AlgoExpert/BinarySearchTrees/Easy/bst-traversal/BstTraversal.h
+++ AlgoExpert/BinarySearchTrees/Easy/bst-traversal/BstTraversal.h
@@ -3,4 +3,16 @@
 
 namespace algoExpert::binarySearchTrees {
 	using std::vector;
+	class BST {
+	public:
+		int value;
+		BST* left;
+		BST* right;
+
+		BST(int val);
+	};
+
+	void inOrderTraverse(BST* tree, vector<int>& array);
+	void preOrderTraverse(BST* tree, vector<int>& array);
+	void postOrderTraverse(BST* tree, vector<int>& array);
 }
