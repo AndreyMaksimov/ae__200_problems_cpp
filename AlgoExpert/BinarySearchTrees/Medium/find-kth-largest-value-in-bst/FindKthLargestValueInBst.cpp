9a1b002f2e8e2255bfd54c0fc72dcd5bf6e9bbc1
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-17 21:32:41 +0300

    find-kth-largest-value-in-bst added solution

--- AlgoExpert/BinarySearchTrees/Medium/find-kth-largest-value-in-bst/FindKthLargestValueInBst.cpp
+++ AlgoExpert/BinarySearchTrees/Medium/find-kth-largest-value-in-bst/FindKthLargestValueInBst.cpp
@@ -6,4 +6,44 @@
 #include "FindKthLargestValueInBst.h"
 
 namespace algoExpert::binarySearchTrees {
+    class BST {
+    public:
+        int value;
+        BST* left = nullptr;
+        BST* right = nullptr;
+        static int k;
+        static int answ;
+        static bool found;
+        static int count;
+
+        BST(int value) { this->value = value; }
+
+        // let's go from right to left
+        static void  ReverseOrderTraverse(BST* tree) {
+            if (found || !tree) return;
+            ReverseOrderTraverse(tree->right);
+            if (++count == BST::k)  {
+                BST::answ = tree->value;
+                found = true;
+                return;
+            }
+            ReverseOrderTraverse(tree->left);
+        }
+    };
+
+    int BST::k = 0;
+    int BST::answ = -1;
+    bool BST::found = false;
+    int BST::count = 0;
+
+    int findKthLargestValueInBst(BST* tree, int k) {
+        // Write your code here.
+        BST::k = k;
+        BST::answ = -1;
+        BST::found = false;
+        BST::count = 0;
+
+        BST::ReverseOrderTraverse(tree);
+        return BST::answ ;
+    }
 }
