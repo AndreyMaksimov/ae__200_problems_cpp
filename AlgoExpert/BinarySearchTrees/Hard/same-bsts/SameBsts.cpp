4125f56f03f81ae7f8f02ff18e3ff6d28cbac256
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-27 22:15:39 +0300

    same-bsts added solution

--- AlgoExpert/BinarySearchTrees/Hard/same-bsts/SameBsts.cpp
+++ AlgoExpert/BinarySearchTrees/Hard/same-bsts/SameBsts.cpp
@@ -3,70 +3,44 @@
 // #BinarySearchTrees
 // #Hard
 
-#include <limits>
-#include <unordered_map>
-#include <utility>
-#include <iostream>
 #include "SameBsts.h"
 
 namespace algoExpert::binarySearchTrees {
-    using std::unordered_multimap, std::numeric_limits, std::pair, std::make_pair;
-    using std::cout, std::endl;
+
+    vector<int> left_nodes(const vector<int>& array) {
+        vector<int> result;
+        const auto a_root = array[0];
+        for(auto i=1; i<array.size(); ++i) {
+            const auto a = array[i];
+            if (a < a_root) {
+                result.push_back(a);
+            }
+        }
+        return result;
+    }
+    vector<int> right_nodes(const vector<int>& array) {
+        vector<int> result;
+        const auto a_root = array[0];
+        for(auto i=1; i<array.size(); ++i) {
+            const auto a = array[i];
+            if (a >= a_root) {
+                result.push_back(a);
+            }
+        }
+        return result;
+    }
+    bool same_nodes(vector<int> array1, vector<int> array2) {
+        if (array1.size() < 3) return array1 == array2;
+        bool ans = array1[0] == array2[0] && array1.size() == array2.size();
+        if (!ans) return false;
+        return same_nodes(left_nodes(array1), left_nodes(array2)) &&
+            same_nodes(right_nodes(array1), right_nodes(array2));
+    }
     bool sameBsts(vector<int> arrayOne, vector<int> arrayTwo) {
-        if (arrayOne.empty()) return false;
         const int size = arrayOne.size();
         if (size != arrayTwo.size()) return false;
+        if (size < 3 ) return (arrayOne == arrayTwo);
 
-        int a = arrayOne[0];
-        if (a != arrayTwo[0]) return false;
-
-        unordered_multimap<int, pair<int, int>> value_lefr_right;
-
-        for (int i = 0; i<size-1; ++i) {
-            auto left_right_pair = make_pair(numeric_limits<int>::min(), numeric_limits<int>::max());
-            auto left = false, right = false;
-            for(int j=i+1; j<size && !(left&&right); ++j) {
-                if (arrayOne[j] < arrayOne[i] && !left) {
-                    left_right_pair.first = arrayOne[j];
-                    left = true;
-                }
-                if (arrayOne[j] >= arrayOne[i] && !right) {
-                    left_right_pair.second = arrayOne[j];
-                    right = true;
-                }
-            }
-            cout << "insert: value = " << arrayOne[i] << "\tleft = " << left_right_pair.first << "\tright = "
-            << left_right_pair.second << endl;
-            value_lefr_right.insert({arrayOne[i], left_right_pair});
-        }
-
-        for (int i = 0; i<size-1; ++i) {
-            auto left_right_pair = make_pair(numeric_limits<int>::min(), numeric_limits<int>::max());
-            auto left = false, right = false;
-            for(int j=i+1; j<size && !(left&&right); ++j) {
-                if (arrayTwo[j] < arrayTwo[i] && !left) {
-                    left_right_pair.first = arrayTwo[j];
-                    left = true;
-                }
-                if (arrayTwo[j] >= arrayTwo[i] && !right) {
-                    left_right_pair.second = arrayTwo[j];
-                    right = true;
-                }
-            }
-            cout << "find: value = " << arrayTwo[i] << "\tleft = " << left_right_pair.first << "\tright = "
-            << left_right_pair.second << endl;
-            const auto res = value_lefr_right.find(arrayTwo[i]);
-            if (res == value_lefr_right.end()) return false;
-            for(auto pos=res; pos != value_lefr_right.end(); ++pos) {
-                auto p = pos->second;
-                if (p == left_right_pair) {
-                    value_lefr_right.erase(pos);
-                    break;
-                }
-                return false;
-            }
-        }
-        return true;
-
+        return same_nodes(arrayOne, arrayTwo);
     }
 }
