4150ad98ae8da8cb37ae6af1f93e6f11c2802e07
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-28 09:12:08 +0300

    validate-three-nodes added solution

--- AlgoExpert/BinarySearchTrees/Hard/validate-three-nodes/ValidateThreeNodes.cpp
+++ AlgoExpert/BinarySearchTrees/Hard/validate-three-nodes/ValidateThreeNodes.cpp
@@ -6,8 +6,22 @@
 #include "ValidateThreeNodes.h"
 
 namespace algoExpert::binarySearchTrees {
+
+    bool findInBST(const BST* const node, const int value) {
+        if (node == nullptr) return false;
+        if (value == node->value) return true;
+        if (value < node->value) {
+            if (node->left) return findInBST(node->left, value);
+        }
+        else {
+            if (node->right) return findInBST(node->right, value);
+        }
+        return false;
+    }
     bool validateThreeNodes(BST* nodeOne, BST* nodeTwo, BST* nodeThree) {
-        // Write your code here.
+        const auto value = nodeTwo->value;
+        return findInBST(nodeOne,value) && findInBST(nodeTwo,nodeThree->value) ||
+            (findInBST(nodeThree,value) && findInBST(nodeTwo,nodeOne->value));
         return false;
     }
 }
