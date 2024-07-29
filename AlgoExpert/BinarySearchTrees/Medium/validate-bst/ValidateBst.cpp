4f0a752c3c00ded3eec5e879a28dc70c7210425c
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-29 15:20:42 +0300

    validate-bst minor fix

--- AlgoExpert/BinarySearchTrees/Medium/validate-bst/ValidateBst.cpp
+++ AlgoExpert/BinarySearchTrees/Medium/validate-bst/ValidateBst.cpp
@@ -7,4 +7,14 @@
 
 namespace algoExpert::binarySearchTrees {
     // see solution in *.h file
+    bool validNode(BST*node, int min_val, int max_val) {
+        if (!node) return true;
+        if (node->value < min_val || node->value >= max_val) return false;
+        return validNode(node->left, min_val, node->value) && validNode(node->right, node->value, max_val);
+    }
+
+    bool validateBst(BST* tree) {
+        // Write your code here.
+        return validNode(tree, std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+    }
 }
--- AlgoExpert/BinarySearchTrees/Medium/validate-bst/ValidateBst.h
+++ AlgoExpert/BinarySearchTrees/Medium/validate-bst/ValidateBst.h
@@ -15,15 +15,8 @@ namespace algoExpert::binarySearchTrees {
 	};
 
 
-	bool validNode(BST*node, int min_val, int max_val) {
-		if (!node) return true;
-		if (node->value < min_val || node->value >= max_val) return false;
-		return validNode(node->left, min_val, node->value) && validNode(node->right, node->value, max_val);
-	}
+	bool validNode(BST*node, int min_val, int max_val);
 
-	bool validateBst(BST* tree) {
-		// Write your code here.
-		return validNode(tree, std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
-	}
+	bool validateBst(BST* tree);
 
 }
