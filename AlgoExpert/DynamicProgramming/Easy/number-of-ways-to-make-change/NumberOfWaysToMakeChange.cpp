9d842e316549acf12aa8f01034487f73360b746c
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-29 23:39:19 +0300

    number-of-ways-to-make-change added solution

--- AlgoExpert/DynamicProgramming/Easy/number-of-ways-to-make-change/NumberOfWaysToMakeChange.cpp
+++ AlgoExpert/DynamicProgramming/Easy/number-of-ways-to-make-change/NumberOfWaysToMakeChange.cpp
@@ -2,12 +2,21 @@
 //
 // #DynamicProgramming
 // #Easy
+// https://sicp.sourceacademy.org/chapters/1.2.2.html
 
 #include "NumberOfWaysToMakeChange.h"
 
 namespace algoExpert::dynamicProgramming {
+    // https://sicp.sourceacademy.org/chapters/1.2.2.html
+    // search for "Example: Counting change"
     int numberOfWaysToMakeChange(int n, vector<int> denoms) {
-        // Write your code here.
-        return -1;
+        if (n==0)
+            return 1;
+        else if ( n<0 || denoms.empty())
+            return 0;
+
+        int denom = denoms.front();
+        vector<int> denoms_without_first(denoms.begin() + 1, denoms.end());
+        return numberOfWaysToMakeChange(n-denom, denoms) + numberOfWaysToMakeChange(n, denoms_without_first);
     }
 }
