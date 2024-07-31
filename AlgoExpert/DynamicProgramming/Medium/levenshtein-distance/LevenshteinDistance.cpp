30f2f9e722bc0624ea94533ddab5c1a57e19827f
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-31 17:01:21 +0300

    levenshtein-distance +links

--- AlgoExpert/DynamicProgramming/Medium/levenshtein-distance/LevenshteinDistance.cpp
+++ AlgoExpert/DynamicProgramming/Medium/levenshtein-distance/LevenshteinDistance.cpp
@@ -2,6 +2,9 @@
 //
 // #DynamicProgramming
 // #Medium
+//
+// https://neerc.ifmo.ru/wiki/index.php?title=%D0%97%D0%B0%D0%B4%D0%B0%D1%87%D0%B0_%D0%BE_%D1%80%D0%B0%D1%81%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D0%B8_%D0%94%D0%B0%D0%BC%D0%B5%D1%80%D0%B0%D1%83-%D0%9B%D0%B5%D0%B2%D0%B5%D0%BD%D1%88%D1%82%D0%B5%D0%B9%D0%BD%D0%B0
+// https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance
 
 #include "LevenshteinDistance.h"
 
