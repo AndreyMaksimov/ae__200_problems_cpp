126f529d998126461369b896aa710c176b3f163e
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-07-31 22:07:12 +0300

    longest-string-chain Initial

---
+++ AlgoExpert/DynamicProgramming/VeryHard/longest-string-chain/LongestStringChain.cpp
@@ -0,0 +1,9 @@
+// https://www.algoexpert.io/questions/longest-string-chain
+//
+// #DynamicProgramming
+// #VeryHard
+
+#include "LongestStringChain.h"
+
+namespace algoExpert::dynamicProgramming {
+}
---
+++ AlgoExpert/DynamicProgramming/VeryHard/longest-string-chain/LongestStringChain_test.cpp
@@ -0,0 +1,5 @@
+#include "LongestStringChain.h"
+#include "gtest/gtest.h"
+
+namespace {
+}
