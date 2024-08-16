4f5f47ab5ccec954a918bc8f62b84361f140d059
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-08-16 22:19:48 +0300

    maximize-expression nice Solution (not mine)

--- AlgoExpert/DynamicProgramming/Hard/maximize-expression/MaximizeExpression.cpp
+++ AlgoExpert/DynamicProgramming/Hard/maximize-expression/MaximizeExpression.cpp
@@ -5,11 +5,14 @@
 
 #include <algorithm>
 #include <limits>
+#include <functional>
 #include "MaximizeExpression.h"
 
 namespace algoExpert::dynamicProgramming {
     using std::max, std::numeric_limits;
-    int maximizeExpression(vector<int> array) {
+    using std::function;
+
+    int mySolution(vector<int> array) {
         const auto size = static_cast<int>(array.size());
         if (size < 4) return 0;
 
@@ -49,5 +52,44 @@ namespace algoExpert::dynamicProgramming {
         }
 
         return abcd_max[size-1];
+    }
+
+    // O(kn) Generalized Solution (k = 4)
+    // shared by Adeeb H.S on June 23, 2022
+    // Explanation
+    // Solution for array[x1] - array[x2] + array[x3] - .... +/- array[xk] where x1 < x2 < x3 < ... < xk.
+    // Time complexity - O(n.k)
+    // Space complexity - O(n)
+
+    void helper(const vector<int>& array,
+        vector<int>& dp,
+        const vector<int>& prev,
+        const int i,
+        const int n,
+        const function<int(int, int)>& fn) {
+        if (i==0) dp[i] = array[i];
+        else dp[i] = fn(prev[i - 1], array[i]);
+
+        for(int j = i + 1; j < n; ++j)
+            dp[i] = max(dp[j - 1], fn(prev[j - 1], array[j]));
+    }
+    // TODO: make it works (pass tests) (see screenshot in the dir)
+    int veryNiceSolution(vector<int> array) {
+        int n = array.size(), k = 4;
+        if (n < k) return 0;
+        vector<int> dp(n), prev;
+        vector<function<int(int, int)>> fns = {
+            [](int a, int b) {return a + b; },
+            [](int a, int b) {return a - b; }
+        };
+        for(int i = 0; i < k; ++i) {
+            prev = dp;
+            helper(array, dp, prev, i, n, fns[i % 2]);
+        }
+        return dp[n - 1];
+    }
+    int maximizeExpression(vector<int> array) {
+        return mySolution(array);
+        // return veryNiceSolution(array);
     }
 }
