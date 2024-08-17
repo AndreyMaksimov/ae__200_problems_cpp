c61f0043f9cb252e8952e7277937b64baa17a4e0
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-08-17 13:06:38 +0300

    kadanes-algorithm Solution

--- AlgoExpert/FamousAlgorithms/Medium/kadanes-algorithm/KadanesAlgorithm.cpp
+++ AlgoExpert/FamousAlgorithms/Medium/kadanes-algorithm/KadanesAlgorithm.cpp
@@ -3,11 +3,24 @@
 // #FamousAlgorithms
 // #Medium
 
+// https://en.wikipedia.org/wiki/Maximum_subarray_problem
+
+#include <algorithm>
 #include "KadanesAlgorithm.h"
 
 namespace algoExpert::famousAlgorithms {
+    using std::max;
     int kadanesAlgorithm(vector<int> array) {
-        // Write your code here.
-        return -1;
+        if (array.empty()) return 0;
+
+        auto pos = array.begin();
+        auto max_current = *pos++;
+        auto max_answ = max_current;
+        while (pos != array.end()) {
+            const auto a = *pos++;
+            max_current = max(a, (a + max_current));
+            max_answ = max(max_answ, max_current);
+        }
+        return max_answ;
     }
 }
