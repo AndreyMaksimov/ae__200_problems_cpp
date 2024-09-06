6255f72ef86e020974553e9b50d90a7ce754254c
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-09-06 22:07:36 +0300

    remove-duplicates-from-linked-list Solution

--- AlgoExpert/LinkedLists/Easy/remove-duplicates-from-linked-list/RemoveDuplicatesFromLinkedList.cpp
+++ AlgoExpert/LinkedLists/Easy/remove-duplicates-from-linked-list/RemoveDuplicatesFromLinkedList.cpp
@@ -6,4 +6,31 @@
 #include "RemoveDuplicatesFromLinkedList.h"
 
 namespace algoExpert::linkedLists {
+    // This is an input struct. Do not edit.
+    class LinkedList {
+    public:
+        int value;
+        LinkedList* next = nullptr;
+
+        LinkedList(int value) { this->value = value; }
+    };
+
+    LinkedList* removeDuplicatesFromLinkedList(LinkedList* linkedList) {
+        if (linkedList == nullptr || linkedList->next == nullptr) {
+            return linkedList;
+        }
+        auto ptr = linkedList;
+
+        while (ptr != nullptr) {
+            // check if next value is equal to current one
+            while (ptr->next != nullptr && (ptr->value == ptr->next->value)) {
+                // skip that node if so
+                ptr->next = ptr->next->next;
+            }
+            // regular shift to the next
+            ptr = ptr->next;
+        }
+
+        return linkedList;
+    }
 }
