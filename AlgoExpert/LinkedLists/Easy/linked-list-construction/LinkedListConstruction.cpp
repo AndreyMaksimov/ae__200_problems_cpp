33d7dbc754cb4c472603713c059d98fba054d453
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-09-07 09:12:13 +0300

    linked-list-construction Initial

---
+++ AlgoExpert/LinkedLists/Easy/linked-list-construction/LinkedListConstruction.cpp
@@ -0,0 +1,9 @@
+// https://www.algoexpert.io/questions/linked-list-construction
+//
+// #LinkedLists
+// #Easy
+
+#include "LinkedListConstruction.h"
+
+namespace algoExpert::linkedLists {
+}
---
+++ AlgoExpert/LinkedLists/Easy/linked-list-construction/LinkedListConstruction_test.cpp
@@ -0,0 +1,5 @@
+#include "LinkedListConstruction.h"
+#include "gtest/gtest.h"
+
+namespace {
+}
