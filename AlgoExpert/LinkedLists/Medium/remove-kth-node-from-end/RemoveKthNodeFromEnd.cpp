57dca881a1f8777143b0050222cec29de4e7efae
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-09-09 22:12:40 +0300

    remove-kth-node-from-end Solution

--- AlgoExpert/LinkedLists/Medium/remove-kth-node-from-end/RemoveKthNodeFromEnd.cpp
+++ AlgoExpert/LinkedLists/Medium/remove-kth-node-from-end/RemoveKthNodeFromEnd.cpp
@@ -18,7 +18,25 @@ namespace algoExpert::linkedLists {
         vector<int> getNodesInArray();
     };
 
+    // The problem is quite similar as 'middle-node' one
+    // We'll create a second pointer which will be k steps behind 1-st one
     void removeKthNodeFromEnd(LinkedList* head, int k) {
-        // Write your code here.
+        auto ptr1 = head;
+        auto ptr2 = head;
+        for (int i = 0; i < k; i++) {
+            ptr1 = ptr1->next;
+        }
+        const auto bEndOfList = (ptr1 == nullptr);
+        while (ptr1 != nullptr) {
+            ptr1 = ptr1->next;
+            if (ptr1 != nullptr) ptr2 = ptr2->next;
+        }
+        if (bEndOfList) { // we need substitute a head value with keep pointer
+            head->value = head->next->value;
+            head->next = head->next->next;
+        }
+        else {
+            if (ptr2->next != nullptr) ptr2->next = ptr2->next->next;
+        }
     }
 }
