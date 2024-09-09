0b68bf038df1ce1d0a64ab4e1bb2d531fc4e89d5
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-09-09 20:38:40 +0300

    remove-kth-node-from-end Prototype

--- AlgoExpert/LinkedLists/Medium/remove-kth-node-from-end/RemoveKthNodeFromEnd.cpp
+++ AlgoExpert/LinkedLists/Medium/remove-kth-node-from-end/RemoveKthNodeFromEnd.cpp
@@ -6,4 +6,19 @@
 #include "RemoveKthNodeFromEnd.h"
 
 namespace algoExpert::linkedLists {
+    using namespace std;
+
+    class LinkedList {
+    public:
+        int value;
+        LinkedList* next;
+
+        LinkedList(int value);
+        void addMany(vector<int> values);
+        vector<int> getNodesInArray();
+    };
+
+    void removeKthNodeFromEnd(LinkedList* head, int k) {
+        // Write your code here.
+    }
 }
