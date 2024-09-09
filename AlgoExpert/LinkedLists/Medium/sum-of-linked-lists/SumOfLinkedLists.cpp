0e6d6ae1595177508d3737e0b8ce44eb7f2e864d
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-09-09 22:26:56 +0300

    sum-of-linked-lists Prototype

--- AlgoExpert/LinkedLists/Medium/sum-of-linked-lists/SumOfLinkedLists.cpp
+++ AlgoExpert/LinkedLists/Medium/sum-of-linked-lists/SumOfLinkedLists.cpp
@@ -6,4 +6,18 @@
 #include "SumOfLinkedLists.h"
 
 namespace algoExpert::linkedLists {
-}
+    // This is an input struct. Do not edit.
+    class LinkedList {
+    public:
+        int value;
+        LinkedList* next = nullptr;
+
+        LinkedList(int value) { this->value = value; }
+    };
+
+    LinkedList* sumOfLinkedLists(
+      LinkedList* linkedListOne, LinkedList* linkedListTwo
+    ) {
+        // Write your code here.
+        return nullptr;
+    }}
