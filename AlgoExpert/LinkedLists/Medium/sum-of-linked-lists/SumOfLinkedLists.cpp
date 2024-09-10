a1b8ddaa10333b58dcfbe9353b13103582cf40d0
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-09-10 15:51:57 +0300

    sum-of-linked-lists Solution

--- AlgoExpert/LinkedLists/Medium/sum-of-linked-lists/SumOfLinkedLists.cpp
+++ AlgoExpert/LinkedLists/Medium/sum-of-linked-lists/SumOfLinkedLists.cpp
@@ -15,9 +15,52 @@ namespace algoExpert::linkedLists {
         LinkedList(int value) { this->value = value; }
     };
 
+    LinkedList* Revert(const LinkedList* linkedList) {
+        if (linkedList == nullptr) return nullptr;
+        auto ptr = linkedList;
+        LinkedList* revertedList = nullptr;
+        while (ptr != nullptr) {
+            const auto node = new LinkedList(ptr->value);
+            node->next = revertedList;
+            revertedList = node;
+            ptr = ptr->next;
+        }
+        return revertedList;
+    }
+
+    // void print(const char* name, const LinkedList* linkedList) {
+    //     using std::cout, std::endl;
+    //     cout << name << ": \t";
+    //     for (auto node = linkedList; node != nullptr; node = node->next) {
+    //         cout << node->value << " ";
+    //     }
+    //     cout << endl;
+    // }
+
     LinkedList* sumOfLinkedLists(
       LinkedList* linkedListOne, LinkedList* linkedListTwo
-    ) {
-        // Write your code here.
-        return nullptr;
-    }}
+    )
+    {
+        LinkedList* sum = nullptr;
+        auto d = 0;
+        while (linkedListOne != nullptr || linkedListTwo != nullptr) {
+            const auto d1 = linkedListOne != nullptr ? linkedListOne->value : 0;
+            const auto d2 = linkedListTwo != nullptr ? linkedListTwo->value : 0;
+            // elementary school two numbers addition
+            const auto sd = d + d1 + d2;
+            d = sd / 10;
+            auto s = sd > 9 ? sd % 10 : sd;
+            const auto node = new LinkedList(s);
+            node->next = sum;
+            sum = node;
+            if (linkedListOne != nullptr) linkedListOne = linkedListOne->next;
+            if (linkedListTwo != nullptr) linkedListTwo = linkedListTwo->next;
+        }
+        if (d != 0) {
+            const auto node = new LinkedList(d);
+            node->next = sum;
+            sum = node;
+        }
+        return Revert(sum);
+    }
+}
