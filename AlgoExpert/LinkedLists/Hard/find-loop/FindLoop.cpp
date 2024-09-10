d09d21be2b3cb94a54caa2553d34000291224c34
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-09-10 22:04:56 +0300

    find-loop Initial

---
+++ AlgoExpert/LinkedLists/Hard/find-loop/FindLoop.cpp
@@ -0,0 +1,9 @@
+// https://www.algoexpert.io/questions/find-loop
+//
+// #LinkedLists
+// #Hard
+
+#include "FindLoop.h"
+
+namespace algoExpert::linkedLists {
+}
---
+++ AlgoExpert/LinkedLists/Hard/find-loop/FindLoop_test.cpp
@@ -0,0 +1,5 @@
+#include "FindLoop.h"
+#include "gtest/gtest.h"
+
+namespace {
+}
