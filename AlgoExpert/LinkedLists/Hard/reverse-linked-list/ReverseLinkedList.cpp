e960f588afe96b482bae7da7e74926d72aa7bda9
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-09-10 19:20:58 +0300

    reverse-linked-list Prototype

--- AlgoExpert/LinkedLists/Hard/reverse-linked-list/ReverseLinkedList.cpp
+++ AlgoExpert/LinkedLists/Hard/reverse-linked-list/ReverseLinkedList.cpp
@@ -6,4 +6,19 @@
 #include "ReverseLinkedList.h"
 
 namespace algoExpert::linkedLists {
+    class LinkedList {
+    public:
+        int value;
+        LinkedList* next;
+
+        LinkedList(int value) {
+            this->value = value;
+            this->next = nullptr;
+        }
+    };
+
+    LinkedList* reverseLinkedList(LinkedList* head) {
+        // Write your code here.
+        return nullptr;
+    }
 }
