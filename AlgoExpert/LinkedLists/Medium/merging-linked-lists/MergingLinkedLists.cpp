01d1d76008262eaf2531d2e65a8239803bb3b5d5
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-09-10 17:14:39 +0300

    merging-linked-lists

--- AlgoExpert/LinkedLists/Medium/merging-linked-lists/MergingLinkedLists.cpp
+++ AlgoExpert/LinkedLists/Medium/merging-linked-lists/MergingLinkedLists.cpp
@@ -6,4 +6,20 @@
 #include "MergingLinkedLists.h"
 
 namespace algoExpert::linkedLists {
+    // This is an input struct. Do not edit.
+    class LinkedList {
+    public:
+        int value;
+        LinkedList* next = nullptr;
+
+        LinkedList(int value) { this->value = value; }
+    };
+
+    LinkedList* mergingLinkedLists(
+      LinkedList* linkedListOne, LinkedList* linkedListTwo
+    ) {
+        // Write your code here.
+        return nullptr;
+    }
+
 }
