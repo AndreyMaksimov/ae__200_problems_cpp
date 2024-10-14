a2c263b02babdcbcba73f810ebe612167ea153fb
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-14 21:12:19 +0300

    shift-linked-list Initial

---
+++ AlgoExpert/LinkedLists/Hard/shift-linked-list/ShiftLinkedList.cpp
@@ -0,0 +1,9 @@
+// https://www.algoexpert.io/questions/shift-linked-list
+//
+// #LinkedLists
+// #Hard
+
+#include "ShiftLinkedList.h"
+
+namespace algoExpert::linkedLists {
+}
---
+++ AlgoExpert/LinkedLists/Hard/shift-linked-list/ShiftLinkedList_test.cpp
@@ -0,0 +1,5 @@
+#include "ShiftLinkedList.h"
+#include "gtest/gtest.h"
+
+namespace {
+}
