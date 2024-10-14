3a31a0a168d54ee5e1c0348c44bff1c9d32caaed
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-14 21:40:23 +0300

    shift-linked-list Solution

--- AlgoExpert/LinkedLists/Hard/shift-linked-list/ShiftLinkedList.cpp
+++ AlgoExpert/LinkedLists/Hard/shift-linked-list/ShiftLinkedList.cpp
@@ -18,7 +18,31 @@ namespace algoExpert::linkedLists {
     };
 
     LinkedList* shiftLinkedList(LinkedList* head, int k) {
-        // Write your code here.
-        return nullptr;
+        if (k == 0) return head;
+        int length = 1;
+        decltype(head) tail = head;
+
+        // Count the list's length
+        while (tail->next != nullptr) {
+            ++length;
+            tail = tail->next;
+        }
+        k = -k % length;
+        if (k == 0) return head;
+
+        // make the list looped
+        tail->next = head;
+
+        if (k < 0) k += length;
+        // Run over the loop till the new tail/head pair
+        for (int i = 1; i < k; ++i) {
+            head = head->next;
+        }
+        tail = head;
+        // Make new head & cut between tail & head
+        head = head->next;
+        tail->next = nullptr;
+
+        return head;
     }
 }
