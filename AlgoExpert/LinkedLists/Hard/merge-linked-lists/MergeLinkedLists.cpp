b54adf5d9758a890ff63fbee981ab96a32652371
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-14 19:49:37 +0300

    merge-linked-lists Prototype

--- AlgoExpert/LinkedLists/Hard/merge-linked-lists/MergeLinkedLists.cpp
+++ AlgoExpert/LinkedLists/Hard/merge-linked-lists/MergeLinkedLists.cpp
@@ -6,4 +6,19 @@
 #include "MergeLinkedLists.h"
 
 namespace algoExpert::linkedLists {
+    class LinkedList {
+    public:
+        int value;
+        LinkedList* next;
+
+        LinkedList(int value) {
+            this->value = value;
+            next = nullptr;
+        }
+    };
+
+    LinkedList* mergeLinkedLists(LinkedList* headOne, LinkedList* headTwo) {
+        // Write your code here.
+        return nullptr;
+    }
 }
