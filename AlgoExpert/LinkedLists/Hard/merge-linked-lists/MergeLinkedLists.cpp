9b519662efe9237e2d7cebc0fcd3cf96b36bb881
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-14 20:39:00 +0300

    merge-linked-lists Solution

--- AlgoExpert/LinkedLists/Hard/merge-linked-lists/MergeLinkedLists.cpp
+++ AlgoExpert/LinkedLists/Hard/merge-linked-lists/MergeLinkedLists.cpp
@@ -18,7 +18,40 @@ namespace algoExpert::linkedLists {
     };
 
     LinkedList* mergeLinkedLists(LinkedList* headOne, LinkedList* headTwo) {
-        // Write your code here.
-        return nullptr;
+        decltype(headOne) mergedHead = nullptr;
+        decltype(headOne) mergedTail = nullptr;
+
+        if (headOne->value < headTwo->value) {
+            mergedHead = headOne;
+            headOne = headOne->next;
+        }
+        else {
+            mergedHead = headTwo;
+            headTwo = headTwo->next;
+        }
+        mergedTail = mergedHead;
+        mergedTail->next = nullptr;
+
+        while (headOne != nullptr && headTwo != nullptr) {
+            if (headOne->value < headTwo->value) {
+                mergedTail->next = headOne;
+                headOne = headOne->next;
+            }
+            else {
+                mergedTail->next = headTwo;
+                headTwo = headTwo->next;
+            }
+            mergedTail = mergedTail->next;
+            mergedTail->next = nullptr;
+        }
+
+        if (headOne != nullptr) {
+            mergedTail->next = headOne;
+        }
+        else if (headTwo != nullptr) {
+            mergedTail->next = headTwo;
+        }
+
+        return mergedHead;
     }
 }
