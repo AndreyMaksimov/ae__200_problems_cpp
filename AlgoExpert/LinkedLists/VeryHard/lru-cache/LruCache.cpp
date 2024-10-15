bf1ce98756ba376fe69fc7193d4763ff06288a9b
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-15 17:43:51 +0300

    lru-cache Prototype

--- AlgoExpert/LinkedLists/VeryHard/lru-cache/LruCache.cpp
+++ AlgoExpert/LinkedLists/VeryHard/lru-cache/LruCache.cpp
@@ -6,4 +6,24 @@
 #include "LruCache.h"
 
 namespace algoExpert::linkedLists {
+    class LRUCache {
+    public:
+        int maxSize;
+
+        LRUCache(int maxSize) { this->maxSize = maxSize > 1 ? maxSize : 1; }
+
+        void insertKeyValuePair(string key, int value) {
+            // Write your code here.
+        }
+
+        int* getValueFromKey(string key) {
+            // Write your code here.
+            return nullptr;
+        }
+
+        string getMostRecentKey() {
+            // Write your code here.
+            return "";
+        }
+    };
 }
