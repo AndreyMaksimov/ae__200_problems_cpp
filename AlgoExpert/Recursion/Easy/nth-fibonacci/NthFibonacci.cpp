6cdb5367c35a7b9055770ceb2811714d34161547
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-16 12:08:47 +0300

    nth-fibonacci Solution

--- AlgoExpert/Recursion/Easy/nth-fibonacci/NthFibonacci.cpp
+++ AlgoExpert/Recursion/Easy/nth-fibonacci/NthFibonacci.cpp
@@ -6,4 +6,9 @@
 #include "NthFibonacci.h"
 
 namespace algoExpert::recursion {
+    int getNthFib(int n) {
+        if (n < 2) return 0;
+        if (n==2) return 1;
+        return getNthFib(n-1) + getNthFib(n-2);
+    }
 }
