da0b71708845e08d512424ca2bb1ce3721ab8e4c
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-16 12:06:51 +0300

    nth-fibonacci Initial 2

---
+++ AlgoExpert/Recursion/Easy/nth-fibonacci/NthFibonacci.cpp
@@ -0,0 +1,9 @@
+// https://www.algoexpert.io/questions/nth-fibonacci
+//
+// #Recursion
+// #Easy
+
+#include "NthFibonacci.h"
+
+namespace algoExpert::recursion {
+}
---
+++ AlgoExpert/Recursion/Easy/nth-fibonacci/NthFibonacci_test.cpp
@@ -0,0 +1,5 @@
+#include "NthFibonacci.h"
+#include "gtest/gtest.h"
+
+namespace {
+}
