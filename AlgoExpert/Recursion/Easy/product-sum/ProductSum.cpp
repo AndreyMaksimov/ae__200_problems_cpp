26caeda8a52ab474c54dcb44c0bc52f10a09bb22
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-21 13:32:07 +0300

    product-sum Prototype

--- AlgoExpert/Recursion/Easy/product-sum/ProductSum.cpp
+++ AlgoExpert/Recursion/Easy/product-sum/ProductSum.cpp
@@ -3,7 +3,19 @@
 // #Recursion
 // #Easy
 
+#include <any>
 #include "ProductSum.h"
 
 namespace algoExpert::recursion {
+    // Tip: You can use el.type() == typeid(vector<any>) to check whether an item is
+    // a list or an integer.
+    // If you know an element of the array is vector<any> you can cast it using:
+    //     any_cast<vector<any>>(element)
+    // If you know an element of the array is an int you can cast it using:
+    //     any_cast<int>(element)
+    using std::any;
+    int productSum(vector<any> array) {
+        // Write your code here.
+        return -1;
+    }
 }
