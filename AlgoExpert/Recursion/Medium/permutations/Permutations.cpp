f38900e4cd04c58bf6895f01bf2275e81c13f313
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-21 21:11:44 +0300

    permutations Solution

--- AlgoExpert/Recursion/Medium/permutations/Permutations.cpp
+++ AlgoExpert/Recursion/Medium/permutations/Permutations.cpp
@@ -3,7 +3,36 @@
 // #Recursion
 // #Medium
 
+#include <vector>
+#include <algorithm>
 #include "Permutations.h"
 
+#include <iterator>
+
 namespace algoExpert::recursion {
+    using std::vector, std::copy_if;
+    void do_permutations(vector<int> v_src, vector<vector<int>>& result, vector<int> current) {
+        if (v_src.size() > 1) {
+            for(int i = 0; i < v_src.size(); i++) {
+                auto current_new = current; // fork existing vector (its head)
+                const auto a = v_src[i];
+                current_new.push_back(a);
+                vector<int> v_new;
+                copy_if(v_src.begin(), v_src.end(), back_inserter(v_new),
+                    [&v_src, &a](int item){return item != a;});
+                do_permutations(v_new, result, current_new);
+            }
+        }
+        else {
+            current.push_back({v_src[0]});
+            result.push_back(current);
+        }
+    }
+    vector<vector<int>> getPermutations(vector<int> array) {
+        if (array.empty()) return {};
+        vector<vector<int>>result;
+        vector<int> current;
+        do_permutations(array, result, current);
+        return result;
+    }
 }
