3c78ead6ddad30d19f93d5b41ed6df1b9df80e4b
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-23 19:24:47 +0300

    powerset Solution

--- AlgoExpert/Recursion/Medium/powerset/Powerset.cpp
+++ AlgoExpert/Recursion/Medium/powerset/Powerset.cpp
@@ -3,12 +3,30 @@
 // #Recursion
 // #Medium
 
+#include <algorithm>
 #include <vector>
 #include "Powerset.h"
 
 namespace algoExpert::recursion {
+    using std::vector, std::copy_if;
+    using vit_t = std::vector<int>::iterator ;
+    void powerset_helper(vit_t& iter, const vit_t& end, vector<vector<int>>& result) {
+        if (iter == end) return;
+        vector<vector<int>> to_append;
+        const auto& a = *iter;
+        for (auto r : result) { // make copy for each existing vector in result
+            r.push_back(a); // add new item from array to each new copy
+            to_append.push_back(r);
+        }
+        // append new vectors to result
+        result.insert(result.end(), to_append.begin(), to_append.end());
+        powerset_helper(++iter, end, result);
+    }
     vector<vector<int>> powerset(vector<int> array) {
         vector<vector<int>>result = {{}};
+        auto iter = array.begin();
+        const auto& end = array.end();
+        powerset_helper(iter, end, result);
         return result;
     }
 }
