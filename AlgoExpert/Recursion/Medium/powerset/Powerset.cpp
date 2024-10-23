bceef0c8a7370c2ab41cf90afe1b551690c720a0
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-23 18:40:22 +0300

    powerset Prototype

--- AlgoExpert/Recursion/Medium/powerset/Powerset.cpp
+++ AlgoExpert/Recursion/Medium/powerset/Powerset.cpp
@@ -3,7 +3,12 @@
 // #Recursion
 // #Medium
 
+#include <vector>
 #include "Powerset.h"
 
 namespace algoExpert::recursion {
+    vector<vector<int>> powerset(vector<int> array) {
+        vector<vector<int>>result = {{}};
+        return result;
+    }
 }
