a02dca3ff22fb9b837a8e70f339541cc3aa37ffb
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-28 20:37:58 +0300

    lowest-common-manager prototype

--- AlgoExpert/Recursion/Hard/lowest-common-manager/LowestCommonManager.cpp
+++ AlgoExpert/Recursion/Hard/lowest-common-manager/LowestCommonManager.cpp
@@ -6,4 +6,23 @@
 #include "LowestCommonManager.h"
 
 namespace algoExpert::recursion {
+    class OrgChart {
+    public:
+        char name;
+        vector<OrgChart*> directReports;
+
+        OrgChart(char name) {
+            this->name = name;
+            this->directReports = {};
+        }
+
+        void addDirectReports(vector<OrgChart*> directReports);
+    };
+
+    OrgChart* getLowestCommonManager(
+      OrgChart* topManager, OrgChart* reportOne, OrgChart* reportTwo
+    ) {
+        // Write your code here.
+        return nullptr;
+    }
 }
