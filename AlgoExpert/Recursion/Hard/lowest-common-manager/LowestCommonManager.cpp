c6b373c38032d6fc36eecd1a87f52b671261de59
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-28 20:36:03 +0300

    lowest-common-manager Initial

---
+++ AlgoExpert/Recursion/Hard/lowest-common-manager/LowestCommonManager.cpp
@@ -0,0 +1,9 @@
+// https://www.algoexpert.io/questions/lowest-common-manager
+//
+// #Recursion
+// #Hard
+
+#include "LowestCommonManager.h"
+
+namespace algoExpert::recursion {
+}
---
+++ AlgoExpert/Recursion/Hard/lowest-common-manager/LowestCommonManager_test.cpp
@@ -0,0 +1,5 @@
+#include "LowestCommonManager.h"
+#include "gtest/gtest.h"
+
+namespace {
+}
