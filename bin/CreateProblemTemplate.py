57335661d749da05874be84ff40a8064f0b675b2
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-28 20:34:43 +0300

    Recursion/Hard

--- bin/CreateProblemTemplate.py
+++ bin/CreateProblemTemplate.py
@@ -4,7 +4,7 @@ import os
 ALGOEXPERT_QUESTIONS_URL = 'https://www.algoexpert.io/questions/'
 ALGOEXPERT_ROOT_DIR = 'AlgoExpert'
 CATEGORY = 'Recursion'
-DIFFICULTY = 'Medium'
+DIFFICULTY = 'Hard'
 PROBLEM_NAME = 'reveal-minesweeper'
 
 CMAKE_LISTS = 'CMakeLists.txt'
