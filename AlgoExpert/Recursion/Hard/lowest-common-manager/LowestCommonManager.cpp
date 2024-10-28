60dbc84221aea70c275eca4e54b5ab1a0672c49a
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-28 22:23:31 +0300

    lowest-common-manager Solution

--- AlgoExpert/Recursion/Hard/lowest-common-manager/LowestCommonManager.cpp
+++ AlgoExpert/Recursion/Hard/lowest-common-manager/LowestCommonManager.cpp
@@ -3,9 +3,11 @@
 // #Recursion
 // #Hard
 
+#include <list>
 #include "LowestCommonManager.h"
 
 namespace algoExpert::recursion {
+    using std::list;
     class OrgChart {
     public:
         char name;
@@ -19,10 +21,33 @@ namespace algoExpert::recursion {
         void addDirectReports(vector<OrgChart*> directReports);
     };
 
+    bool find_reporter(OrgChart* pMgr, OrgChart* pReporter, list<OrgChart*>& listTo) {
+        for (auto rp : pMgr->directReports) {
+            if ( (rp == pReporter) || find_reporter(rp, pReporter, listTo)) {
+                listTo.push_front(rp); // we will save way from top manager to the reporter
+                return true;
+            }
+        }
+        return false;
+    }
+
     OrgChart* getLowestCommonManager(
-      OrgChart* topManager, OrgChart* reportOne, OrgChart* reportTwo
-    ) {
-        // Write your code here.
-        return nullptr;
+      OrgChart* topManager, OrgChart* reportOne, OrgChart* reportTwo)
+    {
+        list<OrgChart*> listOne;
+        list<OrgChart*> listTwo;
+
+        find_reporter(topManager, reportOne, listOne);
+        find_reporter(topManager, reportTwo, listTwo);
+
+        auto rp1 = listOne.begin();
+        auto rp2 = listTwo.begin();
+        auto answ = topManager;
+        while (rp1 != listOne.end() && rp2 != listTwo.end()) {
+            if (*rp1 != *rp2) break;
+            answ = *rp1++;
+            ++rp2;
+        }
+        return answ;
     }
 }
