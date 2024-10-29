8ded55c96af3bc86759bd028111ec5dda20b0900
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-10-29 19:12:28 +0300

    interweaving-strings Prototype

--- AlgoExpert/Recursion/Hard/interweaving-strings/InterweavingStrings.cpp
+++ AlgoExpert/Recursion/Hard/interweaving-strings/InterweavingStrings.cpp
@@ -6,4 +6,8 @@
 #include "InterweavingStrings.h"
 
 namespace algoExpert::recursion {
+    bool interweavingStrings(string one, string two, string three) {
+        // Write your code here.
+        return false;
+    }
 }
--- AlgoExpert/Recursion/Hard/interweaving-strings/InterweavingStrings.h
+++ AlgoExpert/Recursion/Hard/interweaving-strings/InterweavingStrings.h
@@ -4,4 +4,5 @@
 
 namespace algoExpert::recursion {
 	using std::vector, std::string;
+	bool interweavingStrings(string one, string two, string three);
 }
