9dcb248ccaf1ae28ee504efb78e578a3e01e1cd6
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-11-06 15:45:20 +0300

    generate-div-tags Initial

---
+++ AlgoExpert/Recursion/Hard/generate-div-tags/GenerateDivTags.cpp
@@ -0,0 +1,9 @@
+// https://www.algoexpert.io/questions/generate-div-tags
+//
+// #Recursion
+// #Hard
+
+#include "GenerateDivTags.h"
+
+namespace algoExpert::recursion {
+}
---
+++ AlgoExpert/Recursion/Hard/generate-div-tags/GenerateDivTags_test.cpp
@@ -0,0 +1,5 @@
+#include "GenerateDivTags.h"
+#include "gtest/gtest.h"
+
+namespace {
+}
