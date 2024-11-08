68576f7f7b988bd2aafd8823986b3d29a433ce87
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-11-08 11:27:01 +0300

    generate-div-tags Solution

--- AlgoExpert/Recursion/Hard/generate-div-tags/GenerateDivTags.cpp
+++ AlgoExpert/Recursion/Hard/generate-div-tags/GenerateDivTags.cpp
@@ -6,8 +6,45 @@
 #include "GenerateDivTags.h"
 
 namespace algoExpert::recursion {
+
+    const string s_open = "<div>";
+    const string s_close = "</div>";
+
+    void gdt_helper(
+        string current,
+        const bool b_open, // true if we enter toe add "open tag"; false - for "close tag"
+        int open_left,
+        int close_left,
+        vector<string>& result) {
+        if (b_open) {
+            if (open_left > 0) {
+                current += s_open;
+                --open_left;
+                ++close_left;
+            }
+        }
+        else {
+            if (close_left > 0) {
+                current += s_close;
+                --close_left;
+            }
+        }
+        if (open_left == 0 && close_left == 0) {
+            result.push_back(current);
+            return;
+        }
+        if (open_left > 0) gdt_helper(current, true, open_left, close_left, result);
+        if (close_left > 0) gdt_helper(current, false, open_left, close_left, result);
+    }
+
     vector<string> generateDivTags(int numberOfTags) {
-        // Write your code here.
-        return {};
+        vector<string> result;
+        if (numberOfTags > 0) gdt_helper(
+            "",
+            true,
+            numberOfTags,
+            0,
+            result);
+        return result;
     }
 }
