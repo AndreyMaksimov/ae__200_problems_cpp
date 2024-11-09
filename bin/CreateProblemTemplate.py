2ffa0f56b0d40479738ee44a60124d1f6484e785
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-11-09 21:41:38 +0300

    Recursion/VeryHard

--- bin/CreateProblemTemplate.py
+++ bin/CreateProblemTemplate.py
@@ -4,7 +4,7 @@ import os
 ALGOEXPERT_QUESTIONS_URL = 'https://www.algoexpert.io/questions/'
 ALGOEXPERT_ROOT_DIR = 'AlgoExpert'
 CATEGORY = 'Recursion'
-DIFFICULTY = 'Hard'
+DIFFICULTY = 'VeryHard'
 PROBLEM_NAME = 'ambiguous-measurements'
 
 CMAKE_LISTS = 'CMakeLists.txt'
