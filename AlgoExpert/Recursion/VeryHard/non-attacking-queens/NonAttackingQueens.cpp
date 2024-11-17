3aed2cf11ec9563cd0952781823e976e80f75584
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-11-17 08:48:21 +0300

    non-attacking-queens Initial

---
+++ AlgoExpert/Recursion/VeryHard/non-attacking-queens/NonAttackingQueens.cpp
@@ -0,0 +1,9 @@
+// https://www.algoexpert.io/questions/non-attacking-queens
+//
+// #Recursion
+// #VeryHard
+
+#include "NonAttackingQueens.h"
+
+namespace algoExpert::recursion {
+}
---
+++ AlgoExpert/Recursion/VeryHard/non-attacking-queens/NonAttackingQueens_test.cpp
@@ -0,0 +1,5 @@
+#include "NonAttackingQueens.h"
+#include "gtest/gtest.h"
+
+namespace {
+}
