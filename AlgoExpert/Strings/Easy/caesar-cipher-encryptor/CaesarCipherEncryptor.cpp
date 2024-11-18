8856eb4c22ac02eb9967f0df36f984685bca0da1
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-11-18 09:51:29 +0300

    caesar-cipher-encryptor Prototype

--- AlgoExpert/Strings/Easy/caesar-cipher-encryptor/CaesarCipherEncryptor.cpp
+++ AlgoExpert/Strings/Easy/caesar-cipher-encryptor/CaesarCipherEncryptor.cpp
@@ -6,4 +6,8 @@
 #include "CaesarCipherEncryptor.h"
 
 namespace algoExpert::strings {
+    string caesarCypherEncryptor(string str, int key) {
+        // Write your code here.
+        return "";
+    }
 }
--- AlgoExpert/Strings/Easy/caesar-cipher-encryptor/CaesarCipherEncryptor.h
+++ AlgoExpert/Strings/Easy/caesar-cipher-encryptor/CaesarCipherEncryptor.h
@@ -4,4 +4,5 @@
 
 namespace algoExpert::strings {
 	using std::vector, std::string;
+	string caesarCypherEncryptor(string str, int key);
 }
