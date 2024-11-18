8f494583fe3d2acacb9d0441c94798ff93999f2c
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-11-18 16:03:11 +0300

    run-length-encoding Solution

--- AlgoExpert/Strings/Easy/run-length-encoding/RunLengthEncoding.cpp
+++ AlgoExpert/Strings/Easy/run-length-encoding/RunLengthEncoding.cpp
@@ -7,7 +7,34 @@
 
 namespace algoExpert::strings {
     string runLengthEncoding(string str) {
-        // Write your code here.
-        return "";
+        // Interesting point:
+        // Benefits of vector<char> over string?
+        // https://stackoverflow.com/questions/11358879/benefits-of-vectorchar-over-string
+        string result; // So probably vector<char> would be better?
+        auto iter = str.begin();
+        auto a_prev = *iter++;
+        int n_prev = 1;
+        while (iter != str.end()) {
+            const auto& a = *iter++;
+            if (a != a_prev) {
+                if (n_prev != 0) {  // n_prev = 0 in case when len of previous chain is exactly = 9
+                    result.push_back(n_prev + 0x30); // '0' = 0x30, '1' - 0x31 ...
+                    result.push_back(a_prev);
+                }
+                a_prev = a;
+                n_prev = 0;
+            }
+            ++n_prev;
+            if (n_prev == 9) {
+                result.push_back('9');
+                result.push_back(a_prev);
+                n_prev = 0;
+            }
+        }
+        if (n_prev > 0) {
+            result.push_back(n_prev + 0x30);
+            result.push_back(a_prev);
+        }
+        return result;
     }
 }
