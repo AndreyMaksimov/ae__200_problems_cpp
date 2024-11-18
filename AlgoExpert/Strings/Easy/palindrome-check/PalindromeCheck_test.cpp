397dfef1b4251da20aa665cfa2b55384570749cc
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-11-18 06:17:28 +0300

    palindrome-check Tests

--- AlgoExpert/Strings/Easy/palindrome-check/PalindromeCheck_test.cpp
+++ AlgoExpert/Strings/Easy/palindrome-check/PalindromeCheck_test.cpp
@@ -1,5 +1,62 @@
 #include "PalindromeCheck.h"
 #include "gtest/gtest.h"
 
-namespace {
+namespace
+{
+    TEST(PalindromeCheck, Case01)
+    {
+        std::string str = "abcdcba";
+        const auto expected = true;
+        const auto output = algoExpert::strings::isPalindrome(str);
+        EXPECT_EQ(expected, output);
+    }
+    TEST(PalindromeCheck, Case02)
+    {
+        std::string str = "a";
+        const auto expected = true;
+        const auto output = algoExpert::strings::isPalindrome(str);
+        EXPECT_EQ(expected, output);
+    }
+    TEST(PalindromeCheck, Case03)
+    {
+        std::string str = "aba";
+        const auto expected = true;
+        const auto output = algoExpert::strings::isPalindrome(str);
+        EXPECT_EQ(expected, output);
+    }
+    TEST(PalindromeCheck, Case05)
+    {
+        std::string str = "abb";
+        const auto expected = false;
+        const auto output = algoExpert::strings::isPalindrome(str);
+        EXPECT_EQ(expected, output);
+    }
+    TEST(PalindromeCheck, Case06)
+    {
+        std::string str = "abba";
+        const auto expected = true;
+        const auto output = algoExpert::strings::isPalindrome(str);
+        EXPECT_EQ(expected, output);
+    }
+    TEST(PalindromeCheck, Case07)
+    {
+        std::string str = "abcdefghhgfedcba";
+        const auto expected = true;
+        const auto output = algoExpert::strings::isPalindrome(str);
+        EXPECT_EQ(expected, output);
+    }
+    TEST(PalindromeCheck, Case08)
+    {
+        std::string str = "abcdefghihgfedcba";
+        const auto expected = true;
+        const auto output = algoExpert::strings::isPalindrome(str);
+        EXPECT_EQ(expected, output);
+    }
+    TEST(PalindromeCheck, Case09)
+    {
+        std::string str = "abcdefghihgfeddcba";
+        const auto expected = false;
+        const auto output = algoExpert::strings::isPalindrome(str);
+        EXPECT_EQ(expected, output);
+    }
 }
