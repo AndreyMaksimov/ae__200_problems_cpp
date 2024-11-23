a052f331cdc7221ae84695472fc0b38e3f401f7b
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-11-23 23:06:57 +0300

    generate-document Solution

--- AlgoExpert/Strings/Easy/generate-document/GenerateDocument.cpp
+++ AlgoExpert/Strings/Easy/generate-document/GenerateDocument.cpp
@@ -3,11 +3,22 @@
 // #Strings
 // #Easy
 
+#include <unordered_map>
 #include "GenerateDocument.h"
 
 namespace algoExpert::strings {
+    using char_n_t = std::unordered_map<char, int>;
     bool generateDocument(string characters, string document) {
-        // Write your code here.
-        return false;
+        char_n_t char_collection;
+        for (const auto& ch : characters) {
+            char_collection[ch] += 1;
+        }
+        for (const auto& ch : document) {
+            const auto it = char_collection.find(ch);
+            if (it == char_collection.end()) return false;
+            it->second -= 1;
+            if (it->second == 0) char_collection.erase(it);
+        }
+        return true;
     }
 }
