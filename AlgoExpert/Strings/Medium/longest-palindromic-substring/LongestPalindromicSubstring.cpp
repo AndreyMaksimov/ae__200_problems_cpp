113b5ef010acf45157f1e8a63acb3ba5a79bb8c1
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-11-27 23:07:25 +0300

    longest-palindromic-substring Solution

--- AlgoExpert/Strings/Medium/longest-palindromic-substring/LongestPalindromicSubstring.cpp
+++ AlgoExpert/Strings/Medium/longest-palindromic-substring/LongestPalindromicSubstring.cpp
@@ -3,11 +3,38 @@
 // #Strings
 // #Medium
 
+#include <algorithm>
 #include "LongestPalindromicSubstring.h"
 
 namespace algoExpert::strings {
+    using str_iter_t = std::string::iterator;
+    bool is_palindrome(const string& str) {
+        auto reverse_string = str;
+        std::reverse(reverse_string.begin(), reverse_string.end());
+        return str == reverse_string;
+    }
+    string same_head(str_iter_t it1, str_iter_t end1, str_iter_t it2, str_iter_t end2) {
+        str_iter_t begin1 = it1;
+        // try to find out the longest contiguous segment with equal chars from the head of both strings
+        while (it1 != end1 && it2 != end2 && *it1 == *it2 ) {
+            ++it1; ++it2;
+        }
+        return {begin1, it1};
+    }
     string longestPalindromicSubstring(string str) {
-        // Write your code here.
-        return "";
+        auto reverse_str = str;
+        std::reverse(reverse_str.begin(), reverse_str.end());
+        auto end1 = str.end();
+        auto end2 = reverse_str.end();
+        string result;
+        for (auto it1 = str.begin(); it1 != end1; ++it1) {
+            for (auto it2 = reverse_str.begin(); it2 != end2; ++it2) {
+                if (*it1 == *it2) { // possible start of palindrome
+                    const auto str = same_head(it1, end1, it2, end2);
+                    if (str.size() > result.size() && is_palindrome(str)) result = str;
+                }
+            }
+        }
+        return result;
     }
 }
