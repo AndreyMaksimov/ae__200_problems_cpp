bd7c1d986d73631edb93983f1e92eb0c8a5f21f7
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-11-28 10:20:14 +0300

    longest-palindromic-substring Improve complexity

--- AlgoExpert/Strings/Medium/longest-palindromic-substring/LongestPalindromicSubstring.cpp
+++ AlgoExpert/Strings/Medium/longest-palindromic-substring/LongestPalindromicSubstring.cpp
@@ -31,7 +31,11 @@ namespace algoExpert::strings {
             for (auto it2 = reverse_str.begin(); it2 != end2; ++it2) {
                 if (*it1 == *it2) { // possible start of palindrome
                     const auto str = same_head(it1, end1, it2, end2);
-                    if (str.size() > result.size() && is_palindrome(str)) result = str;
+                    if (str.size() > result.size() && is_palindrome(str)) {
+                        result = str;
+                    }
+                    // let's skip a segment that we just passed:
+                    it2 += str.size() - 1; // that improves CPU from O(n**3) to O(n**2)
                 }
             }
         }
