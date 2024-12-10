13299361e6b5e8a5a7f40cda2a3d1445dd979acf
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-12-10 19:33:20 +0300

    reverse-words-in-string Solution

--- AlgoExpert/Strings/Medium/reverse-words-in-string/ReverseWordsInString.cpp
+++ AlgoExpert/Strings/Medium/reverse-words-in-string/ReverseWordsInString.cpp
@@ -3,11 +3,40 @@
 // #Strings
 // #Medium
 
+#include <list>
 #include "ReverseWordsInString.h"
 
 namespace algoExpert::strings {
+    using words_chain_t = std::list<std::string>;
     string reverseWordsInString(string str) {
-        // Write your code here.
-        return "";
+        if (str.empty()) return "";
+        words_chain_t words;
+        bool b_space = str[0] == ' ';
+        string word_maker;
+        for (const auto &c : str) {
+            // we'll switch between type of char and collect words in revers order (push_front)
+            if ( c == ' ') {
+                if (!b_space) {
+                    words.push_front(word_maker);
+                    word_maker.clear();
+                    b_space = true;
+                }
+            }
+            else {
+                if (b_space) {
+                    words.push_front(word_maker);
+                    word_maker.clear();
+                    b_space = false;
+                }
+            }
+            word_maker.push_back(c);
+        }
+        words.push_front(word_maker);
+
+        string reversed_words;
+        for (const auto &word : words) {
+            reversed_words.append(word);
+        }
+        return reversed_words;
     }
 }
