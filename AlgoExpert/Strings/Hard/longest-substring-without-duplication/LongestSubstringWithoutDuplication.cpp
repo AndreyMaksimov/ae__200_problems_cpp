43ca09d860d68e95d98c54e1472d6606e9383103
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-12-11 20:13:35 +0300

    longest-substring-without-duplication Solution

--- AlgoExpert/Strings/Hard/longest-substring-without-duplication/LongestSubstringWithoutDuplication.cpp
+++ AlgoExpert/Strings/Hard/longest-substring-without-duplication/LongestSubstringWithoutDuplication.cpp
@@ -5,38 +5,34 @@
 
 #include <unordered_map>
 #include <algorithm>
-#include <iostream>
 #include "LongestSubstringWithoutDuplication.h"
 
 namespace algoExpert::strings {
-    using std::cout, std::endl;
     using char2pos_t = std::unordered_map<char, int>;
     string longestSubstringWithoutDuplication(string str) {
+        // The idea is to create a map to keep track previous position of the char
         char2pos_t char2pos;
         int pos = 0;
-        int from_pos = 0;
-        int max_len_from_pos = 0;
-        int prev_pos = 0;
+        int from_pos_max = 0;
         int max_len = 0;
+        int from_pos_current = 0;
         int current_len = 0;
         for (const auto &c : str) {
             const auto& it = char2pos.find(c);
             if (it != char2pos.end()) {
-                prev_pos = it->second;
-                if (prev_pos >= from_pos) {
-                    from_pos = it->second + 1;
-                    current_len = pos - from_pos;
+                const auto& prev_pos = it->second;
+                if (prev_pos >= from_pos_current) {
+                    from_pos_current = it->second + 1;
+                    current_len = pos - from_pos_current;
                 }
             }
             ++current_len;
             if (current_len >= max_len) {
-                max_len_from_pos = from_pos;
+                from_pos_max = from_pos_current;
                 max_len = current_len;
             }
-            cout << pos << "  from = " << max_len_from_pos << "  max_len = " << max_len << endl;
-            char2pos[c] = pos;
-            ++pos;
+            char2pos[c] = pos++;
         }
-        return str.substr(max_len_from_pos, max_len);
+        return str.substr(from_pos_max, max_len);
     }
 }
