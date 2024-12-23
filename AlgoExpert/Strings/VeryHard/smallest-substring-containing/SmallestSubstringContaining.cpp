35f2d2cdb336a1ff77b9e82df7b0bf8bac888171
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-12-23 22:30:45 +0300

    smallest-substring-containing Check if small is a subset of big

--- AlgoExpert/Strings/VeryHard/smallest-substring-containing/SmallestSubstringContaining.cpp
+++ AlgoExpert/Strings/VeryHard/smallest-substring-containing/SmallestSubstringContaining.cpp
@@ -3,11 +3,26 @@
 // #Strings
 // #VeryHard
 
+#include <unordered_map>
 #include "SmallestSubstringContaining.h"
 
 namespace algoExpert::strings {
+    using char_count_t = std::unordered_map<char, int>; // chars counter in a string
+
     string smallestSubstringContaining(string bigString, string smallString) {
-        // Write your code here.
+        char_count_t bigMap, smallMap;
+        for (const auto &c : smallString) smallMap[c]++;
+        for (const auto &c : bigString) bigMap[c]++;
+
+        // Step1: check if smallMap is a subset of bigMap
+        for (auto itSmall = smallMap.begin(); itSmall != smallMap.end(); ++itSmall) {
+            const auto& c = itSmall->first;
+            const auto& itBig = bigMap.find(c);
+            if (itBig == bigMap.end()) return "";
+            const auto& smallCount = itSmall->second;
+            const auto& bigCount = itBig->second;
+            if (bigCount < smallCount) return "";
+        }
         return "";
     }
 }
