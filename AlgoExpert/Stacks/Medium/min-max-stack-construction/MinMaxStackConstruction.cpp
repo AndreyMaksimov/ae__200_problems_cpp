1f1c93ec488d3203ac1bf06638e353ef8d750ae9
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-12-29 22:28:08 +0300

    min-max-stack-construction Initial

---
+++ AlgoExpert/Stacks/Medium/min-max-stack-construction/MinMaxStackConstruction.cpp
@@ -0,0 +1,9 @@
+// https://www.algoexpert.io/questions/min-max-stack-construction
+//
+// #Stacks
+// #Medium
+
+#include "MinMaxStackConstruction.h"
+
+namespace algoExpert::stacks {
+}
---
+++ AlgoExpert/Stacks/Medium/min-max-stack-construction/MinMaxStackConstruction_test.cpp
@@ -0,0 +1,5 @@
+#include "MinMaxStackConstruction.h"
+#include "gtest/gtest.h"
+
+namespace {
+}
