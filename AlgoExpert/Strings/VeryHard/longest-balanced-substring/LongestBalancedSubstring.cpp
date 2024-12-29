723e91562b290e12b73a462a7fc0d7bc4549215f
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-12-29 10:21:18 +0300

    longest-balanced-substring Solution

--- AlgoExpert/Strings/VeryHard/longest-balanced-substring/LongestBalancedSubstring.cpp
+++ AlgoExpert/Strings/VeryHard/longest-balanced-substring/LongestBalancedSubstring.cpp
@@ -3,14 +3,9 @@
 // #Strings
 // #VeryHard
 
-#include <iostream>
 #include "LongestBalancedSubstring.h"
 
 namespace algoExpert::strings {
-    using size_str_t = std::string::size_type;
-    using std::cout, std::endl;
-    using str_iter_t = string::iterator;
-
     /**
      * Replaces all unmatched parenthesis by '.'
      * Parameters below depend on forward or reverse direction
@@ -54,61 +49,30 @@ namespace algoExpert::strings {
     }
 
     int longestBalancedSubstring(string str) {
-        cout << "IN: " << str << endl;
-        auto str_lr(str);
-        pointUnmatchedParenthesis(str_lr, false);
-        cout << "LR: " << str_lr << endl;
-        auto str_rl(str);
-        pointUnmatchedParenthesis(str_rl, true);
-        cout << "RL: " << str_rl << endl;
-        pointUnmatchedParenthesis(str_lr, true);
-        cout << "..: " << str_lr << endl;
 
-        const auto len = str.size();
-        if (len < 2) return 0;
+        // Example how pointUnmatchedParenthesis works (test case #36)
+        // reverse = false : we go from left to right
+        // reverse = true  : we go from right to left
+        // Original string : ()()))))(())((()(()(
+        // reverse = false : ()()....(())((()(()(
+        // reverse = true  : ()()))))(())..().().
+        // false, then true: ()()....(())..().().
 
-        size_str_t i_start = 0, i_end = len - 1;
-        auto skip_close_parenthesis = [&](size_str_t from_idx)
-        {
-            while (str[from_idx] == ')') {
-                if (++from_idx == len - 1) break;
+        pointUnmatchedParenthesis(str, false);
+        pointUnmatchedParenthesis(str, true);
+
+        // So the only thing left to do is find the longest range between points
+        // Actually it can be done together with 2-nd pass of pointUnmatchedParenthesis
+        int max_balance_len = 0;
+        int len = 0;
+        for (const auto& c : str) {
+            if (c != '.') len++;
+            else {
+                if (len > max_balance_len) max_balance_len = len;
+                len = 0;
             }
-            return from_idx;
-        };
-        i_start = skip_close_parenthesis(0);
-        while (str[i_end] == '(') {
-            if (--i_end == 0) break;
         }
-        if (i_end - i_start < 1) return 0;
-
-        size_str_t max_balance_len = 0;
-        while (i_start < i_end)
-        {
-            int balance_count = 0;
-            auto i = i_start;
-            while (true) {
-                if (str[i] == '(') ++balance_count;
-                else --balance_count;
-                if (balance_count < 0) {
-                    ++balance_count;
-                    --i;
-                    break;
-                }
-                if (i == i_end) break;
-                ++i;
-            }
-            if (balance_count == 0) {
-                const auto balance_len = i - i_start + 1;
-                if (balance_len > max_balance_len) max_balance_len = balance_len;
-            }
-            else if (i == i_end) {
-                const auto balance_len = i - i_start + 1 - balance_count;
-                if (balance_len > max_balance_len) max_balance_len = balance_len;
-                break;
-            }
-            i_start = skip_close_parenthesis(i+1);
-        }
-
+        if (len > max_balance_len) max_balance_len = len;
         return max_balance_len;
     }
 }
