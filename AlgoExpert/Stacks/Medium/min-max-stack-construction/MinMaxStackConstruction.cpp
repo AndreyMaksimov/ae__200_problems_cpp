2ac779ed6cb61b8c61f6b86620dc7eaf9da64807
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-12-30 00:15:42 +0300

    min-max-stack-construction Solution

--- AlgoExpert/Stacks/Medium/min-max-stack-construction/MinMaxStackConstruction.cpp
+++ AlgoExpert/Stacks/Medium/min-max-stack-construction/MinMaxStackConstruction.cpp
@@ -3,33 +3,50 @@
 // #Stacks
 // #Medium
 
+#include <utility>
+#include <algorithm>
 #include "MinMaxStackConstruction.h"
 
 namespace algoExpert::stacks {
+    using min_max_t = std::pair<int, int>;
+    using value_limits_t = std::pair<int, min_max_t>;
+    using stack_t = std::vector<value_limits_t>;
     class MinMaxStack {
+        stack_t m_stack;
     public:
         int peek() {
-            // Write your code here.
-            return -1;
+            return m_stack.back().first;
         }
 
         int pop() {
-            // Write your code here.
-            return -1;
+            const auto back_value = m_stack.back().first;
+            m_stack.pop_back();
+            return back_value;
         }
 
         void push(int number) {
-            // Write your code here.
+            if (m_stack.empty()) {
+                min_max_t min_max(number, number);
+                value_limits_t value_limits(number, min_max);
+                m_stack.push_back(value_limits);
+            }
+            else {
+                const auto& prev_min = m_stack.back().second.first;
+                const auto& prev_max = m_stack.back().second.second;
+                const auto& new_min = std::min(number, prev_min);
+                const auto& new_max = std::max(number, prev_max);
+                min_max_t min_max(new_min, new_max);
+                value_limits_t value_limits(number, min_max);
+                m_stack.push_back(value_limits);
+            }
         }
 
         int getMin() {
-            // Write your code here.
-            return -1;
+            return m_stack.back().second.first;;
         }
 
         int getMax() {
-            // Write your code here.
-            return -1;
+            return m_stack.back().second.second;
         }
     };
 
