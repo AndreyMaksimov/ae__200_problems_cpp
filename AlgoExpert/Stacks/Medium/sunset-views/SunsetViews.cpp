9a39484f2634d5154239d9e33f2d973a3a5f7acf
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-12-30 09:53:45 +0300

    sunset-views Solution

--- AlgoExpert/Stacks/Medium/sunset-views/SunsetViews.cpp
+++ AlgoExpert/Stacks/Medium/sunset-views/SunsetViews.cpp
@@ -3,11 +3,30 @@
 // #Stacks
 // #Medium
 
+#include <algorithm>
 #include "SunsetViews.h"
 
 namespace algoExpert::stacks {
     vector<int> sunsetViews(vector<int> buildings, string direction) {
-        // Write your code here.
-        return {};
+        if (buildings.empty()) return {};
+        const auto reverse  = direction== "EAST";
+        const int i_last= static_cast<int>(buildings.size()) - 1;
+        const auto i_begin = reverse ? i_last : 0;
+        auto i_valid = [&](const int& i){
+            if (reverse) return i >= 0;
+            else return i <= i_last;
+        };
+        const auto i_step = reverse ? -1 : 1;
+
+        vector<int> result;
+        int h_max = 0;
+        for (int i=i_begin; i_valid(i); i += i_step) {
+            if (buildings[i] > h_max) {
+                h_max = buildings[i];
+                result.push_back(i);
+            }
+        }
+        if (reverse) std::reverse(result.begin(), result.end());
+        return result;
     }
 }
