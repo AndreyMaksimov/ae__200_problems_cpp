e407790fe14555efbe5acf643ca4dd0a1b032d4e
Andrey Maksimov <andrey.maksimov@gmail.com>
2024-12-31 00:05:24 +0300

    sort-stack v 0.1

--- AlgoExpert/Stacks/Medium/sort-stack/SortStack.cpp
+++ AlgoExpert/Stacks/Medium/sort-stack/SortStack.cpp
@@ -3,11 +3,69 @@
 // #Stacks
 // #Medium
 
+#include <stack>
+#include <algorithm>
+#include <iostream>
 #include "SortStack.h"
 
 namespace algoExpert::stacks {
-    vector<int> sortStack(vector<int>& stack) {
-        // Write your code here.
-        return {};
+    using std::cout, std::endl;
+
+    void print_vector(const char* title, std::vector<int>& v) {
+        cout << title << ": ";
+        for (const auto& i : v) {
+            cout << '\t' << i << " ";
+        }
+        cout << endl;
+    }
+
+    int stack_sorter(std::stack<int>& st, int value, bool& swap_done) {
+        if (st.empty()) return value;
+        auto next_value = st.top();
+        st.pop();
+        if (next_value < value) {
+            std::swap(value, next_value);
+            swap_done = true;
+        }
+        st.push(stack_sorter(st, next_value, swap_done));
+        return value;
+    }
+
+    void sort_stack(std::stack<int>& st) {
+        if (st.empty()) return;
+        auto a = st.top();
+        st.pop();
+        bool swap_done = false;
+        a = stack_sorter(st, a, swap_done);
+        st.push(a);
+    }
+
+    vector<int> sortStack(vector<int>& array) {
+        std::stack<int> st;
+        for (const auto& element : array) {
+            st.push(element);
+        }
+
+        sort_stack(st);
+        sort_stack(st);
+        sort_stack(st);
+        sort_stack(st);
+        sort_stack(st);
+        sort_stack(st);
+        sort_stack(st);
+        sort_stack(st);
+        sort_stack(st);
+        sort_stack(st);
+
+        vector<int> result;
+        while (!st.empty()) {
+            result.push_back(st.top());
+            st.pop();
+        }
+
+        print_vector(" Input: ", array);
+        // std::reverse(result.begin(), result.end());
+        print_vector("Output: ", result);
+        return result;
     }
 }
