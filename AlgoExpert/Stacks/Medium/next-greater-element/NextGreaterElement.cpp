00014ceed8ee82f3466c20318bce72a78c0bfcca
Andrey Maksimov <andrey.maksimov@gmail.com>
2025-01-01 22:05:13 +0300

    next-greater-element Solution O(n)

--- AlgoExpert/Stacks/Medium/next-greater-element/NextGreaterElement.cpp
+++ AlgoExpert/Stacks/Medium/next-greater-element/NextGreaterElement.cpp
@@ -3,36 +3,26 @@
 // #Stacks
 // #Medium
 
-#include <iostream>
 #include <stack>
-#include <algorithm>
 #include "NextGreaterElement.h"
 
 #include <array>
 
 namespace algoExpert::stacks {
-    using std::cout, std::endl;
-
     vector<int> nextGreaterElement(vector<int> array) {
         if (array.empty()) return {};
         const auto size = array.size();
-        vector<int> result(size);
-        auto a_max = *std::max_element(array.begin(), array.end());
-
-        for (auto i = 0; i < size; i++) {
-            const auto element = array[i];
-            if (element == a_max) {
-                result[i] = -1;
-                continue;
+        vector<int> result(size, -1);
+        std::stack<size_t> cache;
+        for (auto i2 = 0; i2 < size*2; ++i2) { // double pass because of cyclic array
+            const auto i = i2 % size;
+            while (! cache.empty() && array[i] > array[cache.top()]) {
+                const auto j = cache.top();
+                result[j] = array[i];
+                cache.pop();
             }
-            auto i_next = i + 1;
-            if (i_next >= size) i_next = 0;
-            while (element >= array[i_next]) {
-                if (++i_next >= size) i_next = 0;
-            }
-            result[i] = array[i_next];
+            cache.push(i);
         }
-
         return result;
     }
 }
