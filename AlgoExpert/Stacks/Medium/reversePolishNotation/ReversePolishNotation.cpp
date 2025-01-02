faa9d0cf6b751109ecb3c28e6879948a8f155e82
Andrey Maksimov <andrey.maksimov@gmail.com>
2025-01-02 08:41:57 +0300

    reversePolishNotation Solution

--- AlgoExpert/Stacks/Medium/reversePolishNotation/ReversePolishNotation.cpp
+++ AlgoExpert/Stacks/Medium/reversePolishNotation/ReversePolishNotation.cpp
@@ -3,11 +3,40 @@
 // #Stacks
 // #Medium
 
+#include <stack>
 #include "ReversePolishNotation.h"
 
 namespace algoExpert::stacks {
     int reversePolishNotation(vector<string> tokens) {
-        // Write your code here.
-        return -1;
+        std::stack<int> values;
+        int a, b;
+        auto ab = [&]() {b = values.top(); values.pop(); a = values.top(); values.pop();};
+        auto plus  = [&] () {ab(); const auto r = a + b; values.push(r); };
+        auto minus = [&] () {ab(); const auto r = a - b; values.push(r); };
+        auto mult  = [&] () {ab(); const auto r = a * b; values.push(r); };
+        auto div   = [&] () {ab(); const auto r = a / b; values.push(r); };
+        for (const auto& token : tokens) {
+            if (token.size() > 1) {
+                values.push(std::stoi(token));
+                continue;
+            }
+            switch (token[0]) {
+                case '+':
+                    plus();
+                    break;
+                case '-':
+                    minus();
+                    break;
+                case '*':
+                    mult();
+                    break;
+                case '/':
+                    div();
+                    break;
+                default:
+                    values.push(std::stoi(token));
+            }
+        }
+        return values.top();
     }
 }
