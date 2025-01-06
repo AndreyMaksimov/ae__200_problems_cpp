ae582d3928f9b7dc5b61edd1f1f1bbe6550ed5f5
Andrey Maksimov <andrey.maksimov@gmail.com>
2025-01-06 06:46:17 +0300

    largest-rectangle-under-skyline Solution

--- AlgoExpert/Stacks/Hard/largest-rectangle-under-skyline/LargestRectangleUnderSkyline.cpp
+++ AlgoExpert/Stacks/Hard/largest-rectangle-under-skyline/LargestRectangleUnderSkyline.cpp
@@ -3,11 +3,28 @@
 // #Stacks
 // #Hard
 
+#include <algorithm>
 #include "LargestRectangleUnderSkyline.h"
 
 namespace algoExpert::stacks {
+    using std::max;
     int largestRectangleUnderSkyline(vector<int> buildings) {
-        // Write your code here.
-        return -1;
+        vector<int> pillarIndices;
+        int maxArea = 0;
+
+        vector <int> extendedBuildings(buildings);
+        extendedBuildings.push_back(0);
+
+        for(int idx = 0; idx < extendedBuildings.size(); idx++) {
+            const int height = extendedBuildings[idx];
+            while (!pillarIndices.empty() && extendedBuildings[pillarIndices.back()] >= height) {
+                const int pillarHeight = extendedBuildings[pillarIndices.back()];
+                pillarIndices.pop_back();
+                const int width = pillarIndices.empty() ? idx : idx - pillarIndices.back() - 1;
+                maxArea = max(width * pillarHeight, maxArea);
+            }
+            pillarIndices.push_back(idx);
+        }
+        return maxArea;
     }
 }
