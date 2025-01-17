7e121e4208a2be97fe25a12176e07cd7cb9374d1
Andrey Maksimov <andrey.maksimov@gmail.com>
2025-01-17 23:27:46 +0300

    Arrays/Hard update CMakeLists.txt's

--- bin/CreateProblemTemplate.py
+++ bin/CreateProblemTemplate.py
@@ -5,8 +5,8 @@ import os
 ALGOEXPERT_QUESTIONS_URL = 'https://www.algoexpert.io/questions/'
 ALGOEXPERT_ROOT_DIR = 'AlgoExpert'
 CATEGORY = 'Arrays'
-DIFFICULTY = 'Easy'
-PROBLEM_NAME = 'validate-subsequence'
+DIFFICULTY = 'Hard'
+PROBLEM_NAME = 'zigzag-traverse'
 
 CMAKE_LISTS = 'CMakeLists.txt'
 DISCOVER_TESTS = '_discover_tests'
