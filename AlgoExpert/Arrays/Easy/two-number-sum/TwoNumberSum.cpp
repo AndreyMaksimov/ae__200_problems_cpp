409dfbf6339cc736be1ed1bc33fd6f147e949ac6
Andrey Maksimov <andrey.maksimov@gmail.com>
2025-01-27 01:57:59 +0300

    two-number-sum update 1

--- AlgoExpert/Arrays/Easy/two-number-sum/TwoNumberSum.cpp
+++ AlgoExpert/Arrays/Easy/two-number-sum/TwoNumberSum.cpp
@@ -5,8 +5,7 @@
 // #std::vector
 // #std::count
 //
-// https://www.youtube.com/watch?v=veJCKqtusK8
-//
+
 
 #include <vector>
 #include <unordered_set>
@@ -14,25 +13,9 @@
 
 namespace algoExpert::arrays {
 
-    using namespace std;
+    using std::vector, std::unordered_set;
 
-    // cpu: O(N**2), memory: O(1)
-    vector<int> twoNumberSum_n2(vector<int>& array, int targetSum) {
-        vector<int> result;
-        const auto arraySize = array.size();
-        for(int i=0; i<arraySize; ++i) {
-            for(int j=i+1; j<arraySize; ++j) {
-                const auto ai = array[i];
-                const auto aj = array[j];
-                if ((ai+aj) == targetSum) {
-                    return {ai, aj};
-                }
-            }
-        }
-        return {};
-    }
-
-    vector<int> twoNumberSum_n1(const vector<int>& array, const int targetSum) {
+    vector<int> twoNumberSum(const vector<int> array, const int targetSum) {
         std::unordered_set<int> hash;
         for (const auto i: array) {
             if ( (i+i) == targetSum) continue; // skip same value
@@ -44,10 +27,5 @@ namespace algoExpert::arrays {
             return {i, di};
         }
         return {};
-    }
-
-    vector<int> twoNumberSum(vector<int> array, int targetSum) {
-        // return twoNumberSum_n2(array, targetSum);
-        return twoNumberSum_n1(array, targetSum);
     }
 }
