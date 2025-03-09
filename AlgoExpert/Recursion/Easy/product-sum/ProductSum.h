6cf41def0ea4e43d253270f26b013a5304e01937
Andrey Maksimov <andrey.maksimov@gmail.com>
2025-03-10 00:06:19 +0300

    ProductSum: added tests

--- AlgoExpert/Recursion/Easy/product-sum/ProductSum.h
+++ AlgoExpert/Recursion/Easy/product-sum/ProductSum.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <any>
 
 namespace algoExpert::recursion {
 	using std::vector, std::string;
+	using std::any;
+	int productSum(vector<any> array);
 }
--- AlgoExpert/Recursion/Easy/product-sum/ProductSum_test.cpp
+++ AlgoExpert/Recursion/Easy/product-sum/ProductSum_test.cpp
@@ -1,5 +1,35 @@
 #include "ProductSum.h"
 #include "gtest/gtest.h"
+// #Copilot helped
 
 namespace {
-}
+    using namespace algoExpert::recursion;
+    TEST(ProductSumTest, TestCase1) {
+        std::vector<std::any> array = {5, 2, std::vector<std::any>{7, -1}, 3, std::vector<std::any>{6, std::vector<std::any>{-13, 8}, 4}};
+        EXPECT_EQ(productSum(array), 12);
+    }
+
+    TEST(ProductSumTest, TestCase2) {
+        std::vector<std::any> array = {1, 2, 3, 4, 5};
+        EXPECT_EQ(productSum(array), 15);
+    }
+
+    TEST(ProductSumTest, TestCase3) {
+        std::vector<std::any> array = {1, 2, std::vector<std::any>{3}, 4, 5};
+        EXPECT_EQ(productSum(array), 18);
+    }
+
+    TEST(ProductSumTest, TestCase4) {
+        std::vector<std::any> array = {std::vector<std::any>{1, 2}, 3, std::vector<std::any>{4, 5}};
+        EXPECT_EQ(productSum(array), 27);
+    }
+
+    TEST(ProductSumTest, TestCase5) {
+        std::vector<std::any> array = {std::vector<std::any>{std::vector<std::any>{std::vector<std::any>{std::vector<std::any>{5}}}}};
+        EXPECT_EQ(productSum(array), 600);
+    }
+
+    TEST(ProductSumTest, TestCase6) {
+        std::vector<std::any> array = {9, std::vector<std::any>{2, -3, 4}, 1, std::vector<std::any>{1, 1, std::vector<std::any>{1, 1, 1}}, std::vector<std::any>{std::vector<std::any>{std::vector<std::any>{std::vector<std::any>{3, 4, 1}}}, 8}, std::vector<std::any>{1, 2, 3, 4, 5, std::vector<std::any>{6, 7}, -7}, std::vector<std::any>{1, std::vector<std::any>{2, 3, std::vector<std::any>{4, 5}}, std::vector<std::any>{6, 0, std::vector<std::any>{7, 0, -8}}, -7}, std::vector<std::any>{1, -3, 2, std::vector<std::any>{1, -3, 2, std::vector<std::any>{1, -3, 2}, std::vector<std::any>{1, -3, 2, std::vector<std::any>{1, -3, 2}}, std::vector<std::any>{1, -3, 2}}}, -3};
+        EXPECT_EQ(productSum(array), 1351);
+    }}
