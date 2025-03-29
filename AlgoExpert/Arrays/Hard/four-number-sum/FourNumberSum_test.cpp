4950bc43805380983e624e89f503e799ef09f38f
Andrey Maksimov <andrey.maksimov@gmail.com>
2025-03-29 21:59:50 +0300

    FourNumberSum: updated tests

--- AlgoExpert/Arrays/Hard/four-number-sum/FourNumberSum_test.cpp
+++ AlgoExpert/Arrays/Hard/four-number-sum/FourNumberSum_test.cpp
@@ -1,5 +1,7 @@
 #include "FourNumberSum.h"
 #include "gtest/gtest.h"
+#include <aelib//compare.h>
+
 
 namespace
 {
@@ -13,7 +15,7 @@ namespace
                         {7, 6, 1, 2}
                 };
         const auto output = algoExpert::arrays::fourNumberSum(array, targetSum);
-        EXPECT_EQ(expected, output);
+        EXPECT_TRUE(aelib::compare_unsorted_vectors(expected, output));
     }
     TEST(FourNumberSum, Case02)
     {
@@ -24,7 +26,7 @@ namespace
                         {1, 2, 3, 4}
                 };
         const auto output = algoExpert::arrays::fourNumberSum(array, targetSum);
-        EXPECT_EQ(expected, output);
+        EXPECT_TRUE(aelib::compare_unsorted_vectors(expected, output));
     }
     TEST(FourNumberSum, Case03)
     {
@@ -37,7 +39,7 @@ namespace
                         {-2, 2, 3, -3}
                 };
         const auto output = algoExpert::arrays::fourNumberSum(array, targetSum);
-        EXPECT_EQ(expected, output);
+        EXPECT_TRUE(aelib::compare_unsorted_vectors(expected, output));
     }
     TEST(FourNumberSum, Case04)
     {
@@ -51,7 +53,7 @@ namespace
                         {-2, -1, 3, 4}
                 };
         const auto output = algoExpert::arrays::fourNumberSum(array, targetSum);
-        EXPECT_EQ(expected, output);
+        EXPECT_TRUE(aelib::compare_unsorted_vectors(expected, output));
     }
     TEST(FourNumberSum, Case05)
     {
@@ -66,7 +68,7 @@ namespace
                         {22, 11, 2, -5}
                 };
         const auto output = algoExpert::arrays::fourNumberSum(array, targetSum);
-        EXPECT_EQ(expected, output);
+        EXPECT_TRUE(aelib::compare_unsorted_vectors(expected, output));
     }
     TEST(FourNumberSum, Case06)
     {
@@ -83,7 +85,7 @@ namespace
                         {-5, 12, 8, 5}
                 };
         const auto output = algoExpert::arrays::fourNumberSum(array, targetSum);
-        EXPECT_EQ(expected, output);
+        EXPECT_TRUE(aelib::compare_unsorted_vectors(expected, output));
     }
     TEST(FourNumberSum, Case07)
     {
@@ -91,7 +93,7 @@ namespace
         int targetSum = 100;
         std::vector<std::vector<int>> expected = {};
         const auto output = algoExpert::arrays::fourNumberSum(array, targetSum);
-        EXPECT_EQ(expected, output);
+        EXPECT_TRUE(aelib::compare_unsorted_vectors(expected, output));
     }
     TEST(FourNumberSum, Case08)
     {
@@ -107,6 +109,6 @@ namespace
                         {1, 4, 6, -6}
                 };
         const auto output = algoExpert::arrays::fourNumberSum(array, targetSum);
-        EXPECT_EQ(expected, output);
+        EXPECT_TRUE(aelib::compare_unsorted_vectors(expected, output));
     }
 }
